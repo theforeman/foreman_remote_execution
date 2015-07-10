988498782749a519095c0c04043eb1013ed4b8d8
Ivan Nečas <inecas@redhat.com>
2015-07-10 13:41:20 +0200

    Host run over ssh

---
+++ app/lib/actions/remote_execution/host_run.rb
@@ -0,0 +1,30 @@
+module Actions
+  module RemoteExecution
+    class HostRun < Actions::ProxyAction
+
+      include ::Dynflow::Action::Cancellable
+
+      def plan(proxy, hostname, script, options = {})
+        options = { :effective_user => nil }.merge(options)
+        super(proxy, options.merge(:hostname => hostname, :script => script))
+      end
+
+      def proxy_action_name
+        'Proxy::RemoteExecution::Ssh::CommandAction'
+      end
+
+      def on_event(event)
+        super(event)
+        error! _("Script execution failed") if failed_run?
+      end
+
+      def rescue_strategy
+        ::Dynflow::Action::Rescue::Skip
+      end
+
+      def failed_run?
+        proxy_output.nil? || proxy_output[:exit_status] != 0
+      end
+    end
+  end
+end
