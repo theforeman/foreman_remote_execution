abdc1d1450a3a4d9d4c8d23788e99ffa4a40d357
Stephen Benjamin <stbenjam@redhat.com>
2015-07-15 10:03:10 -0400

    Make API controller test consistent

--- test/functional/api/v2/job_templates_controller_test.rb
+++ test/functional/api/v2/job_templates_controller_test.rb
@@ -7,14 +7,14 @@ module Api
         @template = FactoryGirl.create :job_template
       end
 
-      test "should get index" do
+      test 'should get index' do
         get :index
         templates = ActiveSupport::JSON.decode(@response.body)
         assert !templates.empty?, "Should response with template"
         assert_response :success
       end
 
-      test "should get template detail" do
+      test 'should get template detail' do
         get :show,  :id => @template.to_param
         assert_response :success
         template = ActiveSupport::JSON.decode(@response.body)
@@ -22,34 +22,34 @@ module Api
         assert_equal template["name"], @template.name
       end
 
-      test "should create valid" do
+      test 'should create valid' do
         JobTemplate.any_instance.stubs(:valid?).returns(true)
         valid_attrs = { :template => "This is a test template", :name => "RandomName", :provider_type => 'ssh' }
         post :create,  :job_template => valid_attrs
         template = ActiveSupport::JSON.decode(@response.body)
         assert template["name"] == "RandomName"
-        assert_response 200
+        assert_response :success
       end
 
-      test "should not create invalid" do
+      test 'should not create invalid' do
         post :create
         assert_response :unprocessable_entity
       end
 
-      test "should update valid" do
+      test 'should update valid' do
         JobTemplate.any_instance.stubs(:valid?).returns(true)
         put :update,  :id => @template.to_param,
                       :job_template => { :template => "blah" }
         assert_response :ok
       end
 
-      test "should not update invalid" do
+      test 'should not update invalid' do
         put :update,  :id => @template.to_param,
                       :job_template => { :name => "" }
         assert_response :unprocessable_entity
       end
 
-      test "should destroy" do
+      test 'should destroy' do
         delete :destroy,  :id => @template.to_param
         assert_response :ok
         refute JobTemplate.exists?(@template.id)
