5130e76b522402c19ad7853df9159eb841c3e3eb
Ivan Nečas <inecas@redhat.com>
2015-08-12 16:47:22 +0200

    Tests for RunHostsJob

---
+++ test/unit/actions/run_hosts_job_test.rb
@@ -0,0 +1,36 @@
+require "test_plugin_helper"
+
+module ForemanRemoteExecution
+  class RunHostsJobTest <  ActiveSupport::TestCase
+    include Dynflow::Testing
+
+    let(:proxy) { FactoryGirl.build(:smart_proxy) }
+    let(:hostname) { 'myhost.example.com' }
+    let(:script) { 'ping -c 5 redhat.com' }
+    let(:targeting) { FactoryGirl.create(:targeting, :search_query => "name = #{host.name}", :user => User.current) }
+    let(:job_invocation) do
+      FactoryGirl.build(:job_invocation).tap do |invocation|
+        invocation.targeting = targeting
+        invocation.save
+      end
+    end
+    let(:host) { FactoryGirl.create(:host) }
+    let(:action) do
+      create_and_plan_action(Actions::RemoteExecution::RunHostsJob, job_invocation)
+    end
+
+    before do
+      User.current = users :admin
+      action
+    end
+
+    it 'resolves the hosts on targeting in plan phase' do
+      targeting.hosts.must_include(host)
+    end
+
+    it 'triggers the RunHostJob actions on the resolved hosts in run phase' do
+      action.expects(:trigger).with(Actions::RemoteExecution::RunHostJob, job_invocation, host)
+      action.create_sub_plans
+    end
+  end
+end
