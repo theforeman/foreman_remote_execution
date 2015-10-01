45bfa62380a479623a64734a8f28c7a6d4de216e
Marek Hulan <mhulan@redhat.com>
2015-10-01 14:02:28 +0200

    Fixes #12024 - balancer recovers if no proxy is usable

--- app/services/proxy_load_balancer.rb
+++ app/services/proxy_load_balancer.rb
@@ -8,8 +8,8 @@ class ProxyLoadBalancer
   def next(proxies)
     exclude = @tasks.keys + @offline
     @tasks.merge!(get_counts(proxies - exclude))
-    next_proxy = @tasks.select { |proxy, _| proxies.include?(proxy) }.min_by { |_, job_count| job_count }.first
-    @tasks[next_proxy] += 1
+    next_proxy = @tasks.select { |proxy, _| proxies.include?(proxy) }.min_by { |_, job_count| job_count }.try(:first)
+    @tasks[next_proxy] += 1 if next_proxy.present?
     next_proxy
   end
 
@@ -20,12 +20,11 @@ class ProxyLoadBalancer
       begin
         proxy_api = ProxyAPI::ForemanDynflow::DynflowProxy.new(:url => proxy.url)
         result[proxy] = proxy_api.tasks_count('running')
-        result
-      rescue
+      rescue => e
         @offline << proxy
-        Rails.logger.error("Could not fetch task counts from #{proxy}, skipped.")
-        result
+        Foreman::Logging.exception "Could not fetch task counts from #{proxy}, skipped.", e
       end
+      result
     end
   end
 end
