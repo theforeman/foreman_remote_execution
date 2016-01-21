ee877285994fd96067c7778d9e1dfa525af6c1e5
Stephen Benjamin <stephen@bitbin.de>
2016-01-21 10:09:28 -0500

    fixes #13317 - allow mass assignment of subnet rex proxies

--- app/models/concerns/foreman_remote_execution/subnet_extensions.rb
+++ app/models/concerns/foreman_remote_execution/subnet_extensions.rb
@@ -5,6 +5,7 @@ module ForemanRemoteExecution
     included do
       has_many :target_remote_execution_proxies, :as => :target
       has_many :remote_execution_proxies, :dependent => :destroy, :through => :target_remote_execution_proxies
+      attr_accessible :remote_execution_proxies, :remote_execution_proxy_ids
     end
   end
 end
