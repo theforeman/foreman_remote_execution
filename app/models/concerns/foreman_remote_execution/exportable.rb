de7f52a5315a3d72eada07fa22b9aa1560f44c84
Stephen Benjamin <stephen@redhat.com>
2016-03-08 12:40:48 -0500

    fixes #13922 - store custom and normal exporters in one hash

--- app/models/concerns/foreman_remote_execution/exportable.rb
+++ app/models/concerns/foreman_remote_execution/exportable.rb
@@ -11,42 +11,41 @@ module ForemanRemoteExecution
     extend ActiveSupport::Concern
 
     def to_export
-      self.class.exportable_attributes.inject({}) do |hash, attribute|
-        value = export_attr(attribute)
+      self.class.exportable_attributes.keys.inject({}) do |hash, attribute|
+        value = export_attr(attribute, self.class.exportable_attributes[attribute])
         value.blank? ? hash : hash.update(attribute => value)
       end.stringify_keys
     end
 
-    def export_attr(attribute)
-      value = self.send(attribute) if self.respond_to?(attribute)
+    def export_attr(attribute, exporter)
+      value = if exporter.respond_to?(:call)
+                exporter.call(self)
+              elsif self.respond_to?(exporter)
+                self.send(exporter)
+              end
 
-      if self.class.custom_exports.include?(attribute)
-        self.class.custom_exports[attribute].call(self)
-      elsif value.respond_to?(:to_export)
-        value.to_export
-      elsif value.respond_to?(:map)
-        export_iterable(value)
-      else
-        value
-      end
+      value.respond_to?(:map) ? export_iterable(value) : value
     end
 
     def export_iterable(items)
-      items.map do |item|
-        item.respond_to?(:to_export) ? item.to_export : item
-      end
+      items.map { |item| item.respond_to?(:to_export) ? item.to_export : item }
     end
 
     module ClassMethods
-      attr_reader :exportable_attributes, :custom_exports
+      attr_reader :exportable_attributes
 
       # Takes an array of exportable attributes, or a custom exports hash.  The
       # custom exports hash should be a key/proc pair used to export the
       # particular attribute.
       def attr_exportable(*args)
-        @custom_exports = args.last.is_a?(Hash) ? args.pop : {}
-        @exportable_attributes ||= []
-        @exportable_attributes += args + @custom_exports.keys
+        @exportable_attributes ||= {}
+        args.each do |arg|
+          if arg.is_a?(Hash)
+            @exportable_attributes.merge!(arg)
+          else
+            @exportable_attributes.merge!(arg => arg)
+          end
+        end
       end
     end
 
