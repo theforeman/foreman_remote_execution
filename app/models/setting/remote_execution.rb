0d75ef6998f5c0108bb09182128bf2e8fdcade2c
Sean O'Keeffe <seanokeeffe797@gmail.com>
2016-05-27 16:20:30 +0100

    fixes #15179 - Select box for settings (#189)

--- app/models/setting/remote_execution.rb
+++ app/models/setting/remote_execution.rb
@@ -22,7 +22,10 @@ class Setting::RemoteExecution < Setting
                  'root'),
         self.set('remote_execution_effective_user_method',
                  N_('What command should be used to switch to the effective user. One of %s') % SSHExecutionProvider::EFFECTIVE_USER_METHODS.inspect,
-                 'sudo'),
+                 'sudo',
+                 'remote_execution_effective_user_method',
+                 nil,
+                 { :collection => Proc.new {Hash[SSHExecutionProvider::EFFECTIVE_USER_METHODS.map{|method| [method, method]}]} }),
         self.set('remote_execution_sync_templates',
                  N_('Whether we should sync templates from disk when running db:seed.'),
                  true)
