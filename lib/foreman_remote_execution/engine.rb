567d5152d6276bcbd31fa215410e7b720d3359c5
Ivan Nečas <inecas@redhat.com>
2016-08-18 15:42:35 +0200

    Bump version to 1.1.0

--- lib/foreman_remote_execution/engine.rb
+++ lib/foreman_remote_execution/engine.rb
@@ -29,7 +29,7 @@ module ForemanRemoteExecution
 
     initializer 'foreman_remote_execution.register_plugin', before: :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_remote_execution do
-        requires_foreman '>= 1.12'
+        requires_foreman '>= 1.13'
 
         apipie_documented_controllers ["#{ForemanRemoteExecution::Engine.root}/app/controllers/api/v2/*.rb"]
 
