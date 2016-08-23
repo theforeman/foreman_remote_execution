f124bcad2060b390fe7301c62bde882a7e69785f
Himanshu <himanshu@localhost.localdomain>
2016-08-23 10:16:16 +0530

    Fixes #16175 - strong params incorrect for arrays and array of hashes

--- lib/foreman_remote_execution/engine.rb
+++ lib/foreman_remote_execution/engine.rb
@@ -104,7 +104,7 @@ module ForemanRemoteExecution
         # add dashboard widget
         # widget 'foreman_remote_execution_widget', name: N_('Foreman plugin template widget'), sizex: 4, sizey: 1
 
-        parameter_filter Subnet, :remote_execution_proxies, :remote_execution_proxy_ids
+        parameter_filter Subnet, :remote_execution_proxies, :remote_execution_proxy_ids => []
         parameter_filter Nic, :execution
       end
     end
