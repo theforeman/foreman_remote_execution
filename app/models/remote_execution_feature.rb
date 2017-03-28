9a9111fa65f9c220d3c791c3b663bdedb597d655
Marek Hulan <mhulan@redhat.com>
2017-03-28 12:31:29 +0200

    Fixes #19046 - skip unknown feature attribute in migration

--- app/models/remote_execution_feature.rb
+++ app/models/remote_execution_feature.rb
@@ -25,11 +25,19 @@ class RemoteExecutionFeature < ActiveRecord::Base
     return false unless RemoteExecutionFeature.table_exists?
     options.assert_valid_keys(*VALID_OPTIONS)
     options[:host_action_button] = false unless options.key?(:host_action_button)
+
     feature = self.find_by_label(label)
+
+    attributes = { :name => name, :provided_input_names => options[:provided_inputs], :description => options[:description], :host_action_button => options[:host_action_button] }
+    # in case DB does not have the attribute created yet but plugin initializer registers the feature, we need to skip this attribute
+    unless self.attribute_names.include?('host_action_button')
+      attributes.delete(:host_action_button)
+    end
+
     if feature.nil?
-      feature = self.create!(:label => label, :name => name, :provided_input_names => options[:provided_inputs], :description => options[:description], :host_action_button => options[:host_action_button])
+      feature = self.create!({ :label => label }.merge(attributes))
     else
-      feature.attributes = { :name => name, :provided_input_names => options[:provided_inputs], :description => options[:description], :host_action_button => options[:host_action_button] }
+      feature.attributes = attributes
       feature.save if feature.changed?
     end
     return feature
