31fda2bd490749e0de0fc1e1c73685c129aaef18
Adam Ruzicka <aruzicka@redhat.com>
2017-06-15 10:55:04 +0200

    Fixes #19920 - Remove job invocations when deleting tasks

--- app/models/concerns/foreman_remote_execution/foreman_tasks_task_extensions.rb
+++ app/models/concerns/foreman_remote_execution/foreman_tasks_task_extensions.rb
@@ -3,7 +3,7 @@ module ForemanRemoteExecution
     extend ActiveSupport::Concern
 
     included do
-      has_many :job_invocations, :dependent => :nullify, :foreign_key => 'task_id'
+      has_many :job_invocations, :dependent => :destroy, :foreign_key => 'task_id'
     end
   end
 end
