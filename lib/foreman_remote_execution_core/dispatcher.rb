870246d049a7c75de40b4033b590e8d52fc75a90
Adam Ruzicka <aruzicka@redhat.com>
2017-08-05 22:49:05 +0200

    Fixes #17514 - Fallback to interval 1 for script runner

--- lib/foreman_remote_execution_core/dispatcher.rb
+++ lib/foreman_remote_execution_core/dispatcher.rb
@@ -4,7 +4,13 @@ module ForemanRemoteExecutionCore
   class Dispatcher < ::ForemanTasksCore::Runner::Dispatcher
 
     def refresh_interval
-      @refresh_interval ||= ForemanRemoteExecutionCore.settings[:runner_refresh_interval].to_i
+      # For the regular runner we don't want to let the user configure
+      #   the refresh interval so live output keeps working
+      @refresh_interval ||= if ForemanRemoteExecutionCore.settings[:async_ssh]
+                              ForemanRemoteExecutionCore.settings[:runner_refresh_interval].to_i
+                            else
+                              1
+                            end
     end
 
   end
