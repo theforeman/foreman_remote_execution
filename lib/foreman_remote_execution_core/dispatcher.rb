b71151d7d5e027eb8c9e7c42eb2943af2c6bb1c6
Adam Ruzicka <aruzicka@redhat.com>
2017-08-14 13:39:08 +0200

    Fixes #17514 - Revert "Fallback to interval 1 for script runner"
    This reverts commit 870246d049a7c75de40b4033b590e8d52fc75a90.

--- lib/foreman_remote_execution_core/dispatcher.rb
+++ lib/foreman_remote_execution_core/dispatcher.rb
@@ -4,13 +4,7 @@ module ForemanRemoteExecutionCore
   class Dispatcher < ::ForemanTasksCore::Runner::Dispatcher
 
     def refresh_interval
-      # For the regular runner we don't want to let the user configure
-      #   the refresh interval so live output keeps working
-      @refresh_interval ||= if ForemanRemoteExecutionCore.settings[:async_ssh]
-                              ForemanRemoteExecutionCore.settings[:runner_refresh_interval].to_i
-                            else
-                              1
-                            end
+      @refresh_interval ||= ForemanRemoteExecutionCore.settings[:runner_refresh_interval].to_i
     end
 
   end
