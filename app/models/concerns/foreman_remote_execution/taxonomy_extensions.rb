9bb3134fd7c0e6f6d46e083f0cfd85f333ad2711
Adam Ruzicka <aruzicka@redhat.com>
2017-09-18 09:43:53 +0200

    Fixes #20427 - Compatibility fix for rails 5

--- app/models/concerns/foreman_remote_execution/taxonomy_extensions.rb
+++ app/models/concerns/foreman_remote_execution/taxonomy_extensions.rb
@@ -4,13 +4,6 @@ module ForemanRemoteExecution
 
     included do
       has_many :job_templates, :through => :taxable_taxonomies, :source => :taxable, :source_type => 'JobTemplate'
-
-      # FIXME - workaround for #11805 - use before_create for setting
-      # the default templates, remove after it's fixed in upstream
-      # (https://github.com/theforeman/foreman/pull/2723) and we don't
-      # support Foreman 1.9
-      skip_callback :create, :after, :assign_default_templates
-      before_create :assign_default_templates
     end
   end
 end
