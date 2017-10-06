806f03af1a67c85990404e00e6f4250868bc372b
Ivan Necas <inecas@redhat.com>
2017-10-06 11:02:38 +0200

    Fixes #21168 - Workaround for rails5 callbacks (#277)

--- app/models/concerns/foreman_remote_execution/taxonomy_extensions.rb
+++ app/models/concerns/foreman_remote_execution/taxonomy_extensions.rb
@@ -4,6 +4,14 @@ module ForemanRemoteExecution
 
     included do
       has_many :job_templates, :through => :taxable_taxonomies, :source => :taxable, :source_type => 'JobTemplate'
+
+      # TODO: on foreman_version_bump
+      # workaround for #11805 - use before_create for setting
+      # the default templates, remove after it's fixed in upstream
+      # (https://github.com/theforeman/foreman/pull/4890) and gets
+      # into a 1.17 release
+      skip_callback :create, :after, :assign_default_templates, :raise => false
+      before_create :assign_default_templates
     end
   end
 end
