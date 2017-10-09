cffdd07f14630c661dc806223b743a560a299c0b
Ivan Nečas <inecas@redhat.com>
2017-10-09 11:37:17 +0200

    Fixes #21242 - revert using prepend instaed of alias_method_chain
    This is only inteneded for the 1.15 and 1.16, the nightlies should
    keep the change and the proper fix should be done in Katello
    to make it work again.

--- app/helpers/concerns/foreman_remote_execution/hosts_helper_extensions.rb
+++ app/helpers/concerns/foreman_remote_execution/hosts_helper_extensions.rb
@@ -1,7 +1,14 @@
 module ForemanRemoteExecution
   module HostsHelperExtensions
-    def multiple_actions
-      super + [ [_('Schedule Remote Job'), new_job_invocation_path, false] ]
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method_chain(:host_title_actions, :run_button)
+      alias_method_chain :multiple_actions, :remote_execution
+    end
+
+    def multiple_actions_with_remote_execution
+      multiple_actions_without_remote_execution + [ [_('Schedule Remote Job'), new_job_invocation_path, false] ]
     end
 
     def schedule_job_multi_button(*args)
@@ -20,9 +27,9 @@ module ForemanRemoteExecution
       link_to(_('Schedule Remote Job'), new_job_invocation_path(:host_ids => [args.first.id]), :id => :run_button, :class => 'btn btn-default')
     end
 
-    def host_title_actions(*args)
+    def host_title_actions_with_run_button(*args)
       title_actions(button_group(schedule_job_multi_button(*args)))
-      super(*args)
+      host_title_actions_without_run_button(*args)
     end
   end
 end
--- app/helpers/concerns/foreman_remote_execution/job_templates_extensions.rb
+++ app/helpers/concerns/foreman_remote_execution/job_templates_extensions.rb
@@ -1,7 +1,13 @@
 module ForemanRemoteExecution
   module JobTemplatesExtensions
-    def permitted_actions(template)
-      original = super(template)
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method_chain :permitted_actions, :run_button
+    end
+
+    def permitted_actions_with_run_button(template)
+      original = permitted_actions_without_run_button(template)
 
       if template.is_a?(JobTemplate)
         original.unshift(display_link_if_authorized(_('Run'), hash_for_new_job_invocation_path(:template_id => template.id).merge(:authorizer => authorizer))) unless template.snippet
