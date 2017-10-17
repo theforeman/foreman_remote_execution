e0867d1272d917e5da25e05af57954add824aabe
Marek Hulan <mhulan@redhat.com>
2017-10-17 18:12:57 +0200

    Fixes #21367 - rely on core to resolve taxonomies

--- app/controllers/api/v2/job_invocations_controller.rb
+++ app/controllers/api/v2/job_invocations_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V2
     class JobInvocationsController < ::Api::V2::BaseController
       include ::Api::Version2
-      include ::Api::TaxonomyScope
       include ::Foreman::Renderer
 
       before_action :find_optional_nested_object
