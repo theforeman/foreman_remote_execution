c18f448480f571d2056045e9443fdf4ab42751c3
Adam Ruzicka <aruzicka@redhat.com>
2017-10-26 13:23:23 +0200

    Fixes #21477 - Rails/HasManyOrHasOneDependent

--- app/models/job_invocation_task_group.rb
+++ app/models/job_invocation_task_group.rb
@@ -1,6 +1,6 @@
 class JobInvocationTaskGroup < ::ForemanTasks::TaskGroup
 
-  has_one :job_invocation, :foreign_key => :task_group_id
+  has_one :job_invocation, :foreign_key => :task_group_id, :dependent => :nullify
 
   alias resource job_invocation
 
--- app/models/job_template.rb
+++ app/models/job_template.rb
@@ -13,7 +13,7 @@ class JobTemplate < ::Template
   include Parameterizable::ByIdName
 
   audited :allow_mass_assignment => true
-  has_many :audits, :as => :auditable, :class_name => Audited.audit_class.name
+  has_many :audits, :as => :auditable, :class_name => Audited.audit_class.name, :dependent => :nullify
   has_many :all_template_invocations, :dependent => :destroy, :foreign_key => 'template_id', :class_name => 'TemplateInvocation'
   has_many :template_invocations, -> { where('host_id IS NOT NULL') }, :foreign_key => 'template_id'
   has_many :pattern_template_invocations, -> { where('host_id IS NULL') }, :foreign_key => 'template_id', :class_name => 'TemplateInvocation'
