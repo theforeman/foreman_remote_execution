8be16e5c46902aa9810c9539e79ae2ea168f3f33
Adam Ruzicka <aruzicka@redhat.com>
2017-11-09 15:31:30 +0100

    Fixes #21668 - Use the middleware

--- app/lib/actions/remote_execution/run_hosts_job.rb
+++ app/lib/actions/remote_execution/run_hosts_job.rb
@@ -7,12 +7,14 @@ module Actions
       include Actions::RecurringAction
 
       middleware.use Actions::Middleware::BindJobInvocation
+      middleware.use Actions::Middleware::RecurringLogic
+      middleware.use Actions::Middleware::WatchDelegatedProxySubTasks
+
+      class CheckOnProxyActions; end
 
       def queue
         ForemanRemoteExecution::DYNFLOW_QUEUE
       end
-
-      class CheckOnProxyActions; end
 
       def delay(delay_options, job_invocation)
         task.add_missing_task_groups(job_invocation.task_group)
@@ -29,11 +31,6 @@ module Actions
         set_up_concurrency_control job_invocation
         input.update(:job_category => job_invocation.job_category)
         plan_self(:job_invocation_id => job_invocation.id)
-      end
-
-      def initiate
-        world.clock.ping suspended_action, 30, CheckOnProxyActions
-        super
       end
 
       def create_sub_plans
@@ -84,22 +81,8 @@ module Actions
         ::Dynflow::Action::Rescue::Skip
       end
 
-      def poll_proxy_tasks
-        delegated_actions_by_proxy.each do |(url, actions)|
-          results = poll_proxy_actions(url, actions)
-          missing, present = results.partition { |result| result[:result].nil? }
-          notify_missing(missing.map { |action| action[:action] })
-          dispatch_results_to_actions(present) if present.any?
-        end
-      end
-
       def run(event = nil)
-        if event == CheckOnProxyActions
-          poll_proxy_tasks
-          suspend
-        else
-          super unless event == Dynflow::Action::Skip
-        end
+        super unless event == Dynflow::Action::Skip
       end
 
       def humanized_input
@@ -108,42 +91,6 @@ module Actions
 
       def humanized_name
         '%s:' % _(super)
-      end
-
-      private
-
-      def notify_missing(actions)
-        actions.each do |action|
-          world.event action.execution_plan_id,
-                      action.run_step_id,
-                      ::Actions::ProxyAction::ProxyActionMissing.new
-        end
-      end
-
-      def dispatch_results_to_actions(actions)
-        # actions.each do |action|
-        #   id = action.output['proxy_task_id']
-        #   unless results.key?(id)
-        #     world.event(actions.execution_plan_id, action.run_step_id)
-        #   end
-        # end
-      end
-
-      def delegated_actions_by_proxy
-        running = sub_plans('state' => %w(running)).map(&:entry_action)
-        with_delegated = running.select { |action| action.input.key? :delegated_action_id }
-        on_proxy = with_delegated.map do |action|
-          action.planned_actions.find { |planned| planned.id == action.input[:delegated_action_id] }
-        end.select { |action| action.output.key?(:proxy_task_id) }
-        on_proxy.group_by { |action| action.input[:proxy_url] }
-      end
-
-      def poll_proxy_actions(url, actions)
-        proxy = ProxyAPI::ForemanDynflow::DynflowProxy.new(:url => url)
-        results = proxy.task_states(actions.map { |action| action.output[:proxy_task_id] })
-        actions.map do |action|
-          { :action => action, :result => results[action.output[:proxy_task_id]] }
-        end
       end
     end
   end
