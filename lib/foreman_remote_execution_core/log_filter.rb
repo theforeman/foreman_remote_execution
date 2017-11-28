b23f6bed5baa3db01c604e93ee967311fd6bf3eb
Adam Ruzicka <aruzicka@redhat.com>
2017-11-28 14:31:54 +0100

    Fixes #16991 - Allow setting different log levels

---
+++ lib/foreman_remote_execution_core/log_filter.rb
@@ -0,0 +1,13 @@
+module ForemanRemoteExecutionCore
+  class LogFilter < ::Logger
+    def initialize(base_logger)
+      @base_logger = base_logger
+    end
+
+    def add(severity, *args)
+      severity ||= ::Logger::UNKNOWN
+      return true if @base_logger.nil? || severity < @level
+      @base_logger.add(severity, *args)
+    end
+  end
+end
