9944d65b146ffba0ea3cbeba9c413b3432c459fe
Michael Moll <kvedulv@kvedulv.de>
2017-12-25 00:47:25 +0100

    stop using HttpPositionalArguments

--- test/functional/api/v2/foreign_input_sets_controller_test.rb
+++ test/functional/api/v2/foreign_input_sets_controller_test.rb
@@ -11,14 +11,14 @@ module Api
       end
 
       test 'should get index' do
-        get :index, :template_id => @template.id
+        get :index, params: { :template_id => @template.id }
         input_sets = ActiveSupport::JSON.decode(@response.body)
         assert !input_sets.empty?, 'Should respond with input sets'
         assert_response :success
       end
 
       test 'should get input set detail' do
-        get :show, :template_id => @template.to_param, :id => @input_set.to_param
+        get :show, params: { :template_id => @template.to_param, :id => @input_set.to_param }
         assert_response :success
         input_set = ActiveSupport::JSON.decode(@response.body)
         assert !input_set.empty?
@@ -27,34 +27,29 @@ module Api
 
       test 'should create valid' do
         valid_attrs = { :target_template_id => @new_foreign_template.id }
-        post :create, :foreign_input_set => valid_attrs, :template_id => @template.to_param
+        post :create, params: { :foreign_input_set => valid_attrs, :template_id => @template.to_param }
         input_set = ActiveSupport::JSON.decode(@response.body)
         assert_equal input_set['target_template_name'], @new_foreign_template.name
         assert_response :success
       end
 
       test 'should not create invalid' do
-        post :create, :template_id => @template.to_param
+        post :create, params: { :template_id => @template.to_param }
         assert_response :unprocessable_entity
       end
 
       test 'should update valid' do
-        put :update,  :template_id => @template.to_param,
-                      :id => @input_set.to_param,
-                      :foreign_input_set => { :include_all => false }
+        put :update, params: { :template_id => @template.to_param, :id => @input_set.to_param, :foreign_input_set => { :include_all => false } }
         assert_response :ok
       end
 
       test 'should not update invalid' do
-        put :update,  :template_id => @template.to_param,
-                      :id => @input_set.to_param,
-                      :foreign_input_set => { :target_template_id => '' }
+        put :update, params: { :template_id => @template.to_param, :id => @input_set.to_param, :foreign_input_set => { :target_template_id => '' } }
         assert_response :unprocessable_entity
       end
 
       test 'should destroy' do
-        delete :destroy,  :template_id => @template.to_param,
-                          :id => @input_set.to_param
+        delete :destroy, params: { :template_id => @template.to_param, :id => @input_set.to_param }
         assert_response :ok
         refute ForeignInputSet.exists?(@input_set.id)
       end
