af7ed130f94da66e2b9c5172c6937a1d9fa2e895
Adam Ruzicka <aruzicka@redhat.com>
2018-01-23 14:27:44 +0100

    Fixes #21457 - Rails 5.1 compatible migration

--- db/migrate/20171129103615_add_secrets_to_job_invocations.rb
+++ db/migrate/20171129103615_add_secrets_to_job_invocations.rb
@@ -1,4 +1,4 @@
-class AddSecretsToJobInvocations < ActiveRecord::Migration
+class AddSecretsToJobInvocations < ActiveRecord::Migration[4.2]
   def change
     add_column :job_invocations, :password, :string
     add_column :job_invocations, :key_passphrase, :string
