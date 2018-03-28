6c352d54263e0302c6634e7fccc123fff3776ed5
Adam Ruzicka <aruzicka@redhat.com>
2018-03-28 11:28:19 +0200

    Fixes #23042 - Fix broken tests

--- test/unit/concerns/foreman_tasks_cleaner_extensions_test.rb
+++ test/unit/concerns/foreman_tasks_cleaner_extensions_test.rb
@@ -1,6 +1,15 @@
 require 'test_plugin_helper'
 
 class ForemanRemoteExecutionForemanTasksCleanerExtensionsTest < ActiveSupport::TestCase
+  # Apply the same stubbing as in foreman-tasks
+  before do
+    # To stop dynflow from backing up actions, execution_plans and steps
+    ForemanTasks.dynflow.world.persistence.adapter.stubs(:backup_to_csv)
+    ForemanTasks::Cleaner.any_instance.stubs(:say) # Make the tests silent
+    # Hack to make the tests pass due to ActiveRecord shenanigans
+    ForemanTasks::Cleaner.any_instance.stubs(:delete_orphaned_dynflow_tasks)
+  end
+
   it 'tries to delete associated job invocations' do
     job = FactoryBot.create(:job_invocation, :with_task)
     ForemanTasks::Cleaner.new(:filter => "id = #{job.task.id}").delete
