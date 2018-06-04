05d0c75ae58e3a38900497e130233b5eb154dd50
Adam Ruzicka <aruzicka@redhat.com>
2018-06-04 14:24:05 +0200

    Fixes #23163 - Test

---
+++ test/functional/api/v2/template_invocations_controller_test.rb
@@ -0,0 +1,31 @@
+require 'test_plugin_helper'
+
+module Api
+  module V2
+    class TemplateInvocationsControllerTest < ActionController::TestCase
+      setup do
+        @job = FactoryBot.create(:job_invocation, :with_template, :with_task)
+        @template_invocation = @job.template_invocations.first
+      end
+
+      test 'should get template invocations belonging to job invocation' do
+        get :template_invocations, params: { :id => @job.id }
+        invocations = ActiveSupport::JSON.decode(@response.body)
+        invocations['results'].count.must_equal @job.template_invocations.count
+        invocations['total'].must_equal @job.template_invocations.count
+
+        expected_result = {
+          'id'                   => @template_invocation.id,
+          'host_id'              => @template_invocation.host_id,
+          'host_name'            => @template_invocation.host.name,
+          'template_id'          => @template_invocation.template_id,
+          'effective_user'       => @template_invocation.effective_user,
+          'job_invocation_id'    => @job.id,
+          'run_host_job_task_id' => @template_invocation.run_host_job_task_id
+        }
+        invocations['results'].must_equal [expected_result]
+        assert_response :success
+      end
+    end
+  end
+end
