2ccffc9a881a1e6fe479a67478c7a2bbdf8ed2be
Marek Hulan <mhulan@redhat.com>
2018-06-12 07:29:21 +0200

    Fixes #23891 - share all templates to all taxonomies

--- db/seeds.d/70-job_templates.rb
+++ db/seeds.d/70-job_templates.rb
@@ -1,8 +1,12 @@
+organizations = Organization.unscoped.all
+locations = Location.unscoped.all
 User.as_anonymous_admin do
   JobTemplate.without_auditing do
     Dir[File.join("#{ForemanRemoteExecution::Engine.root}/app/views/templates/**/*.erb")].each do |template|
       sync = !Rails.env.test? && Setting[:remote_execution_sync_templates]
-      JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => sync)
+      template = JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => sync)
+      template.organizations = organizations if SETTINGS[:organizations_enabled] && template.present?
+      template.locations = locations if SETTINGS[:locations_enabled] && template.present?
     end
   end
 end
