059545fa6cd0730babb038df1e0e79ff4ba1e200
Adam Růžička <adamruzicka@users.noreply.github.com>
2018-09-19 05:13:56 -0700

    Fixes #22209 - Render empty lines properly in output (#380)
    For some reason the output uses CRLF to mark a newline, writing CRs into
    the terminal window led to line numbers not matching the lines. Also we
    were splitting the output on newlines and trailing empty fields were
    omitted.
    
    There was also an issue when line had color information but was otherwise
    empty. Also multiple consecutive spaces were removed by the browser due
    to missing css property.

--- app/helpers/job_invocation_output_helper.rb
+++ app/helpers/job_invocation_output_helper.rb
@@ -1,4 +1,5 @@
 module JobInvocationOutputHelper
+  COLOR_PATTERN = /\e\[.*?m/
   CONSOLE_COLOR = {
     '31' => 'red',
     '32' => 'lightgreen',
@@ -17,19 +18,19 @@ module JobInvocationOutputHelper
   }.tap { |h| h.default = 'default' }.freeze
 
   def colorize_line(line)
-    line = line.gsub(/\e\[.*?m/) do |seq|
+    line = line.gsub(COLOR_PATTERN) do |seq|
       color = seq[/(\d+)m/,1]
       "{{{format color:#{color}}}}"
     end
 
-    current_color = 'default'
+    @current_color ||= 'default'
     out = %{<span style="color: #{@current_color}">}
     parts = line.split(/({{{format.*?}}})/)
     parts.each do |console_line|
       if console_line.include?('{{{format')
         if (color_index = console_line[/color:(\d+)/, 1]).present?
-          current_color = CONSOLE_COLOR[color_index]
-          out << %{</span><span style="color: #{current_color}">}
+          @current_color = CONSOLE_COLOR[color_index]
+          out << %{</span><span style="color: #{@current_color}">}
         end
       else
         out << h(console_line)
