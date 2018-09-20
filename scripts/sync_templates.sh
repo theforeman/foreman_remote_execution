02e50d0ac457f2f076dde025eb9f25f92c2dd62d
Stephen Benjamin <stephen@redhat.com>
2018-09-20 14:44:36 -0400

    fixes #25000 - fix remote execution template sync script

--- scripts/sync_templates.sh
+++ scripts/sync_templates.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copies job templates from community-templates repository to
 # app/views/templates/ so they can be seeded on new installations.
@@ -7,31 +7,35 @@
 
 REPO=$(mktemp -d)
 trap "rm -rf $REPO" EXIT
+shopt -s extglob
 
 git clone -q -b develop https://github.com/theforeman/community-templates $REPO/ct
 
-# Move into destination dir if run from plugin root
-[ -d app/views/templates ] && cd app/views/templates
+pushd $PWD
+cd $REPO/ct/job_templates
 
-# Add underscore prefix to snippets
-if [ -d $REPO/ct/jobs/snippets ];
-then
-  for i in $REPO/ct/jobs/snippets/*;
-  do
-    mv $i $REPO/ct/jobs/snippets/_$(basename $i)
-  done
-fi
+# Delete all non-ssh templates and remove suffix
+rm !(*ssh_default.erb)
+for i in *.erb
+do
+  mv $i ${i%%_-_ssh_default.erb}.erb
+done
+
+popd
+
+# Move into destination dir if run from plugin root
+[ -d app/views/templates/ssh ] && cd app/views/templates/ssh
 
 rsync -r \
   --exclude README.md \
   --exclude '.*' \
   --exclude test \
   --exclude Rakefile \
-  $REPO/ct/jobs/ ./
+  $REPO/ct/job_templates/*.erb ./
 
 cd -
 
-git status -- app/views/templates
+git status -- app/views/templates/ssh
 
 if [ $(git status --porcelain -u -- app/views/templates | grep -c '^\?') -gt 0 ]; then
   echo
