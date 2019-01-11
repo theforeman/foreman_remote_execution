de306b5abfb178cd3b30619198ec86f9d5bfa75c
Ivan Nečas <inecas@redhat.com>
2019-01-11 15:42:33 +0100

    Refs #24299 - migration to remove proxy-less setting
    We missed this change in the original PR.

---
+++ db/migrate/20190111153330_remove_remote_execution_without_proxy_setting.rb
@@ -0,0 +1,13 @@
+class RemoveRemoteExecutionWithoutProxySetting < ActiveRecord::Migration[5.2]
+  def up
+    Setting.where(name: 'remote_execution_without_proxy').delete_all
+  end
+
+  def down
+    Setting.create!(:name => 'remote_execution_without_proxy',
+                    :description => N_('When enabled, the remote execution will try to run '\
+                                       'the commands directly, when no proxy with remote execution '\
+                                       'feature is configured for the host.'),
+                    :default => false, :full_name => N_('Fallback Without Proxy'))
+  end
+end
