963d345e82f531c4cdf565416e5d51c1bd09d554
Ivan Nečas <inecas@redhat.com>
2019-08-11 22:14:10 +0200

    Refs #26428 - introduce retrieve script timeouts

--- lib/foreman_remote_execution_core/async_scripts/retrieve.sh
+++ lib/foreman_remote_execution_core/async_scripts/retrieve.sh
@@ -14,15 +14,75 @@ URL_PREFIX="$CALLBACK_HOST/dynflow/tasks/$TASK_ID"
 AUTH="$TASK_ID:$OTP"
 CURL="curl --silent --show-error --fail --max-time 10"
 
+MY_LOCK_FILE="$BASE_DIR/retrieve_lock.$$"
+MY_PID=$$
+echo $MY_PID >"$MY_LOCK_FILE"
+LOCK_FILE="$BASE_DIR/retrieve_lock"
+
+RUN_TIMEOUT=30 # for how long can the script hold the lock
+WAIT_TIMEOUT=60 # for how long the script is trying to acquire the lock
+START_TIME=$(date +%s)
+
+fail() {
+    echo RUNNING
+    echo "$1"
+    exit 1
+}
+
+acquire_lock() {
+    # try to acquire lock by creating the file (ln should be atomic an fail in case
+    # another process succeeded first). We also check the content of the lock file,
+    # in case our process won when competing over the lock while invalidating
+    # the lock on timeout.
+    ln "$MY_LOCK_FILE" "$LOCK_FILE" 2>/dev/null || [ "$(head -n1 "$LOCK_FILE")" = "$MY_PID" ]
+    return $?
+}
+
 # acquiring the lock before proceeding, to ensure only one instance of the script is running
-while ! mkfifo "$BASE_DIR/retrieve_lock" 1>/dev/null 2>&1; do
+while ! acquire_lock; do
     # we failed to create retrieve_lock - assuming there is already another retrieve script running
-    # waiting until it finished before we try to acquire it
-    read -t 10 <>"$BASE_DIR/retrieve_lock"
+    current_pid=$(head -n1 "$LOCK_FILE")
+    if [ -z "$current_pid" ]; then
+        continue
+    fi
+    # check whether the lock is not too old (compared to $RUN_TIMEOUT) and try to kill
+    # if it is, so that we don't have a stalled processes here
+    lock_lines_count=$(wc -l < "$LOCK_FILE")
+    current_lock_time=$(stat --format "%Y" "$LOCK_FILE")
+    current_time=$(date +%s)
+
+    if [ "$(( current_time - START_TIME ))" -gt "$WAIT_TIMEOUT" ]; then
+        # We were waiting for the lock for too long - just give up
+        fail "Wait time exceeded $WAIT_TIMEOUT"
+    elif [ "$(( current_time - current_lock_time ))" -gt "$RUN_TIMEOUT" ]; then
+        # The previous lock it hold for too long - re-acquiring procedure
+        if [ "$lock_lines_count" -gt 1 ]; then
+           # there were multiple processes waiting for lock without resolution
+           # longer than the $RUN_TIMEOUT - we reset the lock file and let processes
+           # to compete
+           echo "RETRY" > "$LOCK_FILE"
+        fi
+        if [ "$current_pid" != "RETRY" ]; then
+            # try to kill the currently stalled process
+            kill -9 "$current_pid" 2>/dev/null
+        fi
+        # try to add our process as one candidate
+        echo $MY_PID >> "$LOCK_FILE"
+        if [ "$( head -n2 "$LOCK_FILE" | tail -n1 )" = "$MY_PID" ]; then
+            # our process won the competition for the new lock: it is the first pid
+            # after the original one in the lock file - take ownership of the lock
+            # next iteration only this process will get through
+            echo $MY_PID >"$LOCK_FILE"
+        fi
+    else
+        # still waiting for the original owner to finish
+        sleep 1
+    fi
 done
 
 release_lock() {
-    rm "$BASE_DIR/retrieve_lock"
+    rm "$MY_LOCK_FILE"
+    rm "$LOCK_FILE"
 }
 # ensure the release the lock at exit
 trap "release_lock" EXIT
