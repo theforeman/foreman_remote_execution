59cc61f447d2c85c08a9dcfa98a94ca4346db43a
Tomer Brisker <tbrisker@gmail.com>
2019-09-01 14:26:09 +0300

    Fixes #27746 - Remove taxonomy enabled checks

--- db/seeds.d/70-job_templates.rb
+++ db/seeds.d/70-job_templates.rb
@@ -5,8 +5,8 @@ User.as_anonymous_admin do
     Dir[File.join("#{ForemanRemoteExecution::Engine.root}/app/views/templates/**/*.erb")].each do |template|
       sync = !Rails.env.test? && Setting[:remote_execution_sync_templates]
       template = JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => sync)
-      template.organizations = organizations if SETTINGS[:organizations_enabled] && template.present?
-      template.locations = locations if SETTINGS[:locations_enabled] && template.present?
+      template.organizations = organizations if template.present?
+      template.locations = locations if template.present?
     end
   end
 end
