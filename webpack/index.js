217ccd7b7f401ec75199e1d4f57f68535d9a8072
Ondřej Ezr <oezr@redhat.com>
2020-03-25 19:32:20 +0100

    Fixes #29410 - use web components

--- webpack/index.js
+++ webpack/index.js
@@ -12,18 +12,3 @@ componentRegistry.register({
 });
 
 registerReducer('foremanRemoteExecutionReducers', rootReducer);
-
-if (window.location.href.match(/job_invocations/)) {
-  // When changing job by breadcrumbs, the URL has a trailing forward slash
-  // parseInt fails because of it
-  const uri = new URI(window.location.href.replace(/\/$/, ""));
-  const jobInvocationId = parseInt(uri.filename(), 10);
-
-  const mountJobInvocationContainer = () => {
-    mount('JobInvocationContainer', '#status_chart', {
-      url: `/job_invocations/chart?id=${jobInvocationId}`,
-    });
-  };
-
-  window.addEventListener('DOMContentLoaded', mountJobInvocationContainer);
-}
