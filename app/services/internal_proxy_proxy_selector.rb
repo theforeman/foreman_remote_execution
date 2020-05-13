70a7e1dc5f890e89f29efc40d5c63ce104735372
Adam Ruzicka <aruzicka@redhat.com>
2020-05-13 22:58:05 +0200

    Fixes #29783 - Add forgotten file

---
+++ app/services/internal_proxy_proxy_selector.rb
@@ -0,0 +1,16 @@
+class InternalProxyProxySelector < ::RemoteExecutionProxySelector
+  def initialize
+    # TODO: Remove this once we have a reliable way of determining the internal proxy without katello
+    raise _('Internal proxy selector can only be used if Katello is enabled') unless defined?(::Katello)
+    super
+  end
+
+  def available_proxies(host, provider)
+    # TODO: Once we have a internal proxy marker/feature on the proxy, we can
+    # swap the implementation
+    internal_proxy = ::Katello.default_capsule
+    super.reduce({}) do |acc, (key, proxies)|
+      acc.merge(key => proxies.select { |proxy| proxy == internal_proxy })
+    end
+  end
+end
