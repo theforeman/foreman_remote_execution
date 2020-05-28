c0a795a9c50d495b5aa7c72f3a80eaf259034f0b
Leos Stejskal <lstejska@redhat.com>
2020-05-28 15:18:45 +0200

    Fixes #29901 - Lock default job templates (#500)

--- db/seeds.d/70-job_templates.rb
+++ db/seeds.d/70-job_templates.rb
@@ -4,7 +4,7 @@ User.as_anonymous_admin do
   JobTemplate.without_auditing do
     Dir[File.join("#{ForemanRemoteExecution::Engine.root}/app/views/templates/**/*.erb")].each do |template|
       sync = !Rails.env.test? && Setting[:remote_execution_sync_templates]
-      template = JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => sync)
+      template = JobTemplate.import_raw!(File.read(template), :default => true, :lock => true, :update => sync)
       template.organizations = organizations if template.present?
       template.locations = locations if template.present?
     end
