8fb9e35f1636b963bae2c18882b1e2e9a1b666f3
Leos Stejskal <lstejska@redhat.com>
2020-06-17 10:03:19 +0200

    Fixes #28723 - <Alert /> component

--- webpack/react_app/components/TargetingHosts/TargetingHosts.js
+++ webpack/react_app/components/TargetingHosts/TargetingHosts.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { translate as __ } from 'foremanReact/common/I18n';
-import { LoadingState } from 'patternfly-react';
+import { LoadingState, Alert } from 'patternfly-react';
 import { STATUS } from 'foremanReact/constants';
 import HostItem from './components/HostItem';
-import ApiError from './components/ApiError';
 
 const TargetingHosts = ({ status, items }) => {
   if (status === STATUS.ERROR) {
-    return <ApiError />;
+    return (
+      <Alert type="error">
+        {__(
+          'There was an error while updating the status, try refreshing the page.'
+        )}
+      </Alert>
+    );
   }
 
   return (
--- webpack/react_app/components/TargetingHosts/__tests__/ApiError.test.js
+++
@@ -1,9 +0,0 @@
-import { testComponentSnapshotsWithFixtures } from '@theforeman/test';
-import ApiError from '../components/ApiError';
-
-const fixtures = {
-  'renders ApiError': {},
-};
-
-describe('ApiError', () =>
-  testComponentSnapshotsWithFixtures(ApiError, fixtures));
