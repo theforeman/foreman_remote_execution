45036c2b32a956558aec8c1cafe299a136fb3a69
Suraj Patil <patilsuraj767@gmail.com>
2020-07-06 22:23:28 +0530

    Refs #30188 - Added migration to copy password to new record effective_user_password

--- db/migrate/20200623073022_rename_sudo_password_to_effective_user_password.rb
+++ db/migrate/20200623073022_rename_sudo_password_to_effective_user_password.rb
@@ -2,7 +2,8 @@ class RenameSudoPasswordToEffectiveUserPassword < ActiveRecord::Migration[6.0]
   def up
     rename_column :job_invocations, :sudo_password, :effective_user_password
 
-    password = Setting.find_by(:name => 'remote_execution_sudo_password').value
+    return unless (password = Setting.find_by(:name => 'remote_execution_sudo_password').try(:value))
+
     Setting.find_by(:name => 'remote_execution_effective_user_password').update(value: password)
 
     Setting.find_by(:name => 'remote_execution_sudo_password').delete
@@ -11,7 +12,7 @@ class RenameSudoPasswordToEffectiveUserPassword < ActiveRecord::Migration[6.0]
   def down
     rename_column :job_invocations, :effective_user_password, :sudo_password
 
-    password = Setting.find_by(:name => 'remote_execution_effective_user_password').value
+    return unless (password = Setting.find_by(:name => 'remote_execution_effective_user_password').try(:value))
 
     Setting.create!(name: 'remote_execution_sudo_password', value: password, description: 'Sudo password', category: 'Setting::RemoteExecution', settings_type: 'string', full_name: 'Sudo password',encrypted: true, default: nil)
     Setting.find_by(:name => 'remote_execution_effective_user_password').delete
