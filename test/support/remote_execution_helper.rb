29b91701acf63d039d74020402b260bf51d1e4a2
Leos Stejskal <lstejska@redhat.com>
2020-07-21 15:01:03 +0200

    Fixes #27988 - Remove comment

--- test/support/remote_execution_helper.rb
+++ test/support/remote_execution_helper.rb
@@ -1,6 +1,4 @@
 module RemoteExecutionHelper
-  # RemoteExecutionHelper.expects(:job_invocation_task_buttons).returns([])
-  # doesn't work together with Leapp RemoteExecutionHelperExtension module
   def job_invocation_task_buttons(task)
     return []
   end
