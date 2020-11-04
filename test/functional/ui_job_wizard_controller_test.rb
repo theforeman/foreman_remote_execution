64b433c53fbc23dfc5de6a71e98fcabd6bfe04e7
MariaAga <mariaagaphonchev@gmail.com>
2020-11-04 18:46:39 +0200

    Fixes #31180 - create job wizard controller

---
+++ test/functional/ui_job_wizard_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_plugin_helper'
+
+class UiJobWizardControllerTest < ActionController::TestCase
+  def setup
+    FactoryBot.create(:job_template, :job_category => 'cat1')
+    FactoryBot.create(:job_template, :job_category => 'cat2')
+    FactoryBot.create(:job_template, :job_category => 'cat2')
+  end
+
+  test 'should respond with categories' do
+    get :categories, :params => {}, :session => set_session_user
+    assert_response :success
+    res = JSON.parse @response.body
+    assert_equal ['cat1','cat2'], res['job_categories']
+  end
+end
