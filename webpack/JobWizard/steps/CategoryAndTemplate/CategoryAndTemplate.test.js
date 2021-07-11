fa65abd277369b1b44f597ea6feaca9f45d6c176
MariaAga <mariaagaphonchev@gmail.com>
2021-07-11 18:57:43 +0200

    Refs #31459 - remove snapshot testing in the job wizard

--- webpack/JobWizard/steps/CategoryAndTemplate/CategoryAndTemplate.test.js
+++ webpack/JobWizard/steps/CategoryAndTemplate/CategoryAndTemplate.test.js
@@ -1,52 +1,149 @@
-import { testComponentSnapshotsWithFixtures } from '@theforeman/test';
-import { CategoryAndTemplate } from './CategoryAndTemplate';
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import { fireEvent, screen, render, act } from '@testing-library/react';
+import * as api from 'foremanReact/redux/API';
+import { JobWizard } from '../../JobWizard';
+import * as selectors from '../../JobWizardSelectors';
+import { jobTemplate, jobTemplateResponse } from '../../__tests__/fixtures';
 
-const baseProps = {
-  setJobTemplate: jest.fn(),
-  selectedTemplateID: 190,
-  setCategory: jest.fn(),
-};
-const fixtures = {
-  'renders with props': {
-    ...baseProps,
-    jobCategories: [
-      'Commands',
-      'Ansible Playbook',
-      'Ansible Galaxy',
-      'Ansible Roles Installation',
-    ],
-    jobTemplates: [
-      {
-        id: 190,
-        name: 'ab Run Command - SSH Default clone',
-        job_category: 'Commands',
-        provider_type: 'SSH',
-        snippet: false,
-      },
-      {
-        id: 168,
-        name: 'Ansible Roles - Ansible Default',
-        job_category: 'Ansible Playbook',
-        provider_type: 'Ansible',
-        snippet: false,
-      },
-      {
-        id: 170,
-        name: 'Ansible Roles - Install from git',
-        job_category: 'Ansible Roles Installation',
-        provider_type: 'Ansible',
-        snippet: false,
-      },
-    ],
-    selectedCategory: 'I am a category',
-  },
-  'render with error': {
-    ...baseProps,
-    errors: { allTemplatesError: 'I have an error' },
-  },
-};
+jest.spyOn(api, 'get');
+jest.spyOn(selectors, 'selectJobTemplate');
+jest.spyOn(selectors, 'selectJobTemplates');
+jest.spyOn(selectors, 'selectJobCategories');
+jest.spyOn(selectors, 'selectCategoryError');
+jest.spyOn(selectors, 'selectAllTemplatesError');
+jest.spyOn(selectors, 'selectTemplateError');
 
-describe('CategoryAndTemplate', () => {
-  describe('rendering', () =>
-    testComponentSnapshotsWithFixtures(CategoryAndTemplate, fixtures));
+jest.spyOn(api, 'get');
+const jobCategories = ['Ansible Commands', 'Puppet', 'Services'];
+
+selectors.selectJobCategories.mockImplementation(() => jobCategories);
+
+selectors.selectJobTemplates.mockImplementation(() => [
+  jobTemplate,
+  { ...jobTemplate, id: 2, name: 'template2' },
+]);
+
+api.get.mockImplementation(({ handleSuccess, ...action }) => {
+  if (action.key === 'JOB_TEMPLATES') {
+    handleSuccess &&
+      handleSuccess({
+        data: { results: [jobTemplateResponse.job_template] },
+      });
+  }
+  return { type: 'get', ...action };
+});
+
+const mockStore = configureMockStore([]);
+const store = mockStore({});
+
+describe('Category And Template', () => {
+  it('should select ', async () => {
+    selectors.selectCategoryError.mockImplementation(() => null);
+    selectors.selectAllTemplatesError.mockImplementation(() => null);
+    selectors.selectTemplateError.mockImplementation(() => null);
+    render(
+      <Provider store={store}>
+        <JobWizard />
+      </Provider>
+    );
+
+    expect(screen.queryAllByLabelText('Error')).toHaveLength(0);
+    expect(screen.queryAllByLabelText('failed')).toHaveLength(0);
+    // Category
+    fireEvent.click(
+      screen.getByLabelText('Ansible Commands', { selector: 'button' })
+    );
+    await act(async () => {
+      await fireEvent.click(screen.getByText('Puppet'));
+    });
+    fireEvent.click(screen.getAllByText('Category and Template')[0]); // to remove focus
+    expect(
+      screen.queryAllByLabelText('Ansible Commands', { selector: 'button' })
+    ).toHaveLength(0);
+    expect(
+      screen.queryAllByLabelText('Puppet', { selector: 'button' })
+    ).toHaveLength(1);
+
+    // Template
+    fireEvent.click(
+      screen.getByDisplayValue('template1', { selector: 'button' })
+    );
+    await act(async () => {
+      await fireEvent.click(screen.getByText('template2'));
+    });
+    fireEvent.click(screen.getAllByText('Category and Template')[0]); // to remove focus
+    expect(
+      screen.queryAllByDisplayValue('template1', { selector: 'button' })
+    ).toHaveLength(0);
+    expect(
+      screen.queryAllByDisplayValue('template2', { selector: 'button' })
+    ).toHaveLength(1);
+  });
+  it('category error ', async () => {
+    selectors.selectCategoryError.mockImplementation(() => 'category error');
+    selectors.selectAllTemplatesError.mockImplementation(() => null);
+    selectors.selectTemplateError.mockImplementation(() => null);
+    render(
+      <Provider store={store}>
+        <JobWizard />
+      </Provider>
+    );
+
+    expect(
+      screen.queryAllByText('Categories list failed with:', { exact: false })
+    ).toHaveLength(1);
+
+    expect(
+      screen.queryAllByText('Templates list failed with:', { exact: false })
+    ).toHaveLength(0);
+    expect(
+      screen.queryAllByText('Template failed with:', { exact: false })
+    ).toHaveLength(0);
+  });
+  it('templates error ', async () => {
+    selectors.selectCategoryError.mockImplementation(() => null);
+    selectors.selectAllTemplatesError.mockImplementation(
+      () => 'templates error'
+    );
+    selectors.selectTemplateError.mockImplementation(() => null);
+    render(
+      <Provider store={store}>
+        <JobWizard />
+      </Provider>
+    );
+
+    expect(
+      screen.queryAllByText('Categories list failed with:', { exact: false })
+    ).toHaveLength(0);
+
+    expect(
+      screen.queryAllByText('Templates list failed with:', { exact: false })
+    ).toHaveLength(1);
+    expect(
+      screen.queryAllByText('Template failed with:', { exact: false })
+    ).toHaveLength(0);
+  });
+  it('template error ', async () => {
+    selectors.selectCategoryError.mockImplementation(() => null);
+    selectors.selectAllTemplatesError.mockImplementation(() => null);
+    selectors.selectTemplateError.mockImplementation(() => 'template error');
+    render(
+      <Provider store={store}>
+        <JobWizard />
+      </Provider>
+    );
+
+    expect(
+      screen.queryAllByText('Categories list failed with:', { exact: false })
+    ).toHaveLength(0);
+
+    expect(
+      screen.queryAllByText('Templates list failed with:', { exact: false })
+    ).toHaveLength(0);
+    expect(
+      screen.queryAllByText('Template failed with:', { exact: false })
+    ).toHaveLength(1);
+  });
 });
--- webpack/__mocks__/foremanReact/components/SearchBar.js
+++ webpack/__mocks__/foremanReact/components/SearchBar.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => <div>search</div>;
+const SearchBar = ({ onChange }) => (
+  <input
+    className="foreman-search"
+    onChange={onChange}
+    placeholder="Filter..."
+  />
+);
 export default SearchBar;
+
+SearchBar.propTypes = {
+  onChange: PropTypes.func,
+};
+
+SearchBar.defaultProps = {
+  onChange: () => null,
+};
