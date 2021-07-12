7dd96ce7e7629d3163dbc484631038594d1852ac
Adam Ruzicka <aruzicka@redhat.com>
2021-07-12 15:50:30 +0200

    Completely drop foreman_remote_execution_core

--- lib/foreman_remote_execution_core/actions.rb
+++
@@ -1,6 +0,0 @@
-module ForemanRemoteExecutionCore
-  module Actions
-    require 'smart_proxy_remote_execution_ssh/actions/run_script'
-    RunScript = Proxy::RemoteExecution::Ssh::Actions::RunScript
-  end
-end
