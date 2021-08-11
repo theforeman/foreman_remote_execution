a984bb3a317e02038f7ecd1bf94ffc74f64a2531
Adam Ruzicka <aruzicka@redhat.com>
2021-08-11 17:04:23 +0200

    Fixes #33242 - Allow false boolean parameters to override true settings
    Previously we treated explicitly set to false and unset as the same
    thing.
    
    (cherry picked from commit c9d113b7b729437e52d6542f79732cfe86452008)

--- app/models/remote_execution_provider.rb
+++ app/models/remote_execution_provider.rb
@@ -89,7 +89,8 @@ class RemoteExecutionProvider
     end
 
     def host_setting(host, setting)
-      host.host_param(setting.to_s) || Setting[setting]
+      param_value = host.host_param(setting.to_s)
+      param_value.nil? ? Setting[setting] : param_value
     end
 
     def ssh_password(_host)
