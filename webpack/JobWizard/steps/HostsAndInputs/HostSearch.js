6e7481a8d1b320f5bf7569b46fde0ed68fbfe684
MariaAga <mariaaga@redhat.com>
2021-10-19 23:04:29 +0200

    Fixes #32315 - Add bookmarks to host search in job wizard

--- webpack/JobWizard/steps/HostsAndInputs/HostSearch.js
+++ webpack/JobWizard/steps/HostsAndInputs/HostSearch.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import SearchBar from 'foremanReact/components/SearchBar';
 import { getControllerSearchProps } from 'foremanReact/constants';
+import { getResults } from 'foremanReact/components/AutoComplete/AutoCompleteActions';
+import { TRIGGERS } from 'foremanReact/components/AutoComplete/AutoCompleteConstants';
 import { hostsController, hostQuerySearchID } from '../../JobWizardConstants';
 import { noop } from '../../../helpers';
 
@@ -13,6 +15,18 @@ export const HostSearch = ({ value, setValue }) => {
   useEffect(() => {
     setValue(searchQuery || '');
   }, [setValue, searchQuery]);
+  const dispatch = useDispatch();
+  const setSearch = newSearchQuery => {
+    dispatch(
+      getResults({
+        url: '/hosts/auto_complete_search',
+        searchQuery: newSearchQuery,
+        controller: 'hostsController',
+        trigger: TRIGGERS.INPUT_CHANGE,
+        id: hostQuerySearchID,
+      })
+    );
+  };
 
   const props = getControllerSearchProps(hostsController, hostQuerySearchID);
   return (
@@ -28,6 +42,7 @@ export const HostSearch = ({ value, setValue }) => {
         }}
         onSearch={noop}
         initialQuery={value}
+        onBookmarkClick={search => setSearch(search)}
       />
     </div>
   );
