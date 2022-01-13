1e337b82ec3c4e9ea9411c75fe70b56ce71ac7cf
Amir Fefer <amirfefer@gmail.com>
2022-01-13 20:53:38 +0200

    Fixes #34260 - add rex features dropdown to host page

--- webpack/react_app/components/RecentJobsCard/constants.js
+++ webpack/react_app/components/RecentJobsCard/constants.js
@@ -10,3 +10,4 @@ export const JOB_BASE_URL = '/job_invocations?search=host+%3D+';
 export const JOB_API_URL =
   '/api/job_invocations?order=start_at+DESC&search=targeted_host_id%3D';
 export const JOBS_IN_CARD = 3;
+export const RECENT_JOBS_KEY = { key: 'RECENT_JOBS_KEY' };
