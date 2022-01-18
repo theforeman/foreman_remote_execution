fc80a9cc444caa75550aae6ac883ea735ded2f04
Stejskal Leos <github@stejskalleos.cz>
2022-01-18 12:54:51 +0000

    Fixes #34201 - Use new Foreman <Pagination>

--- webpack/react_app/components/TargetingHosts/TargetingHostsHelpers.js
+++ webpack/react_app/components/TargetingHosts/TargetingHostsHelpers.js
@@ -4,7 +4,7 @@ export const getApiUrl = (searchQuery, pagination) => {
   const baseUrl = getURI()
     .search('')
     .addQuery('page', pagination.page)
-    .addQuery('per_page', pagination.perPage);
+    .addQuery('per_page', pagination.per_page);
 
   return searchQuery ? baseUrl.addQuery('search', searchQuery) : baseUrl;
 };
