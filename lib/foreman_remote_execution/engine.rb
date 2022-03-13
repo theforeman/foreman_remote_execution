a52c0d767c06116cef57987231ae796072e7b10f
Amir Fefer <amirfefer@gmail.com>
2022-03-14 01:04:12 +0200

    Fixes #34600 - add web/cockpit console to host's kebab

--- lib/foreman_remote_execution/engine.rb
+++ lib/foreman_remote_execution/engine.rb
@@ -265,6 +265,7 @@ module ForemanRemoteExecution
         extend_rabl_template 'api/v2/smart_proxies/main', 'api/v2/smart_proxies/pubkey'
         extend_rabl_template 'api/v2/interfaces/main', 'api/v2/interfaces/execution_flag'
         extend_rabl_template 'api/v2/subnets/show', 'api/v2/subnets/remote_execution_proxies'
+        extend_rabl_template 'api/v2/hosts/main', 'api/v2/host/main'
         parameter_filter ::Subnet, :remote_execution_proxy_ids
         describe_host { overview_buttons_provider :host_overview_buttons }
 
--- webpack/global_index.js
+++ webpack/global_index.js
@@ -3,6 +3,7 @@ import routes from './Routes/routes';
 import fillregistrationAdvanced from './react_app/extend/fillregistrationAdvanced';
 import fillRecentJobsCard from './react_app/extend/fillRecentJobsCard';
 import fillFeaturesDropdown from './react_app/extend/fillRexFeaturesDropdown';
+import fillKebabItems from './react_app/extend/fillKebabItems';
 import registerReducers from './react_app/extend/reducers';
 
 registerReducers();
@@ -10,3 +11,4 @@ registerRoutes('foreman_remote_execution', routes);
 fillFeaturesDropdown();
 fillRecentJobsCard();
 fillregistrationAdvanced();
+fillKebabItems();
