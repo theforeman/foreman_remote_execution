b18d3f4be9caf0802bdbb226cb316955cdfef3da
Adam Ruzicka <a.ruzicka@outlook.com>
2022-03-31 11:59:48 +0200

    Fixes #34705 - Introduce Script proxy feature

--- db/seeds.d/60-ssh_proxy_feature.rb
+++ db/seeds.d/60-ssh_proxy_feature.rb
@@ -1,2 +1,5 @@
 f = Feature.where(:name => 'SSH').first_or_create
 raise "Unable to create proxy feature: #{format_errors f}" if f.nil? || f.errors.any?
+
+f = Feature.where(:name => 'Script').first_or_create
+raise "Unable to create proxy feature: #{format_errors f}" if f.nil? || f.errors.any?
