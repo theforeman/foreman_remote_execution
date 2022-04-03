e9d86b406fe1e38b59f4ba724832c4456142e090
Amir Fefer <amirfefer@gmail.com>
2022-04-03 19:35:51 +0300

    Fixes #34665 - unify all fills for keeping consistency

--- webpack/global_index.js
+++ webpack/global_index.js
@@ -1,14 +1,8 @@
 import { registerRoutes } from 'foremanReact/routes/RoutingService';
 import routes from './Routes/routes';
-import fillregistrationAdvanced from './react_app/extend/fillregistrationAdvanced';
-import fillRecentJobsCard from './react_app/extend/fillRecentJobsCard';
-import fillFeaturesDropdown from './react_app/extend/fillRexFeaturesDropdown';
-import fillKebabItems from './react_app/extend/fillKebabItems';
 import registerReducers from './react_app/extend/reducers';
+import registerFills from './react_app/extend/Fills';
 
 registerReducers();
 registerRoutes('foreman_remote_execution', routes);
-fillFeaturesDropdown();
-fillRecentJobsCard();
-fillregistrationAdvanced();
-fillKebabItems();
+registerFills();
