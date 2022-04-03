8ac7af80e31d17442395ebb628fe2c6ddddfec33
Amir Fefer <amirfefer@gmail.com>
2022-04-03 19:35:51 +0300

    Fixes #34665 - unify all fills for keeping consistency

---
+++ webpack/react_app/extend/Fills.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { addGlobalFill } from 'foremanReact/components/common/Fill/GlobalFill';
+
+import FeaturesDropdown from '../components/FeaturesDropdown';
+import RexInterface from '../components/RegistrationExtension/RexInterface';
+import RecentJobsCard from '../components/RecentJobsCard';
+import KebabItems from '../components/HostKebab/KebabItems';
+
+const fills = [
+  {
+    slot: 'host-details-kebab',
+    name: 'kebab-items',
+    component: props => <KebabItems {...props} />,
+    weight: 500,
+  },
+  {
+    slot: 'host-overview-cards',
+    name: 'latest-jobs',
+    component: props => <RecentJobsCard {...props} />,
+    weight: 500,
+  },
+  {
+    slot: 'registrationAdvanced',
+    name: 'interface',
+    component: props => <RexInterface {...props} />,
+    weight: 500,
+  },
+  {
+    slot: '_rex-host-features',
+    name: '_rex-host-features',
+    component: props => <FeaturesDropdown {...props} />,
+    weight: 1000,
+  },
+];
+
+const registerFills = () => {
+  fills.forEach(({ slot, id, component: Component, weight, metadata }) =>
+    addGlobalFill(
+      slot,
+      id,
+      <Component key={`rex-fill-${id}`} />,
+      weight,
+      metadata
+    )
+  );
+};
+
+export default registerFills;
