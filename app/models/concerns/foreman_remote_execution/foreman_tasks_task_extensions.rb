e0f4bb55c0d877782ac74c371866f75b02021de3
Adam Ruzicka <a.ruzicka@outlook.com>
2022-05-06 13:29:56 +0200

    Fixes #34871 - Allow searching on tasks by REX feature

--- app/models/concerns/foreman_remote_execution/foreman_tasks_task_extensions.rb
+++ app/models/concerns/foreman_remote_execution/foreman_tasks_task_extensions.rb
@@ -4,6 +4,12 @@ module ForemanRemoteExecution
 
     included do
       has_many :job_invocations, :dependent => :destroy, :foreign_key => 'task_id'
+      has_one :template_invocation, :inverse_of => :run_host_job_task, :foreign_key => 'run_host_job_task_id', :dependent => :nullify
+      has_one :template, :through => :template_invocation
+      has_many :remote_execution_features, :through => :template
+
+      scoped_search :relation => :remote_execution_features, :on => :name, :rename => 'remote_execution_feature.name'
+      scoped_search :relation => :remote_execution_features, :on => :label, :rename => 'remote_execution_feature.label'
     end
   end
 end
