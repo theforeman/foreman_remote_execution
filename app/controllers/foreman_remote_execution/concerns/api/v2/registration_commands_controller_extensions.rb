6b55c66deef287c4d92e387063e10829a9de9fde
Adam Ruzicka <a.ruzicka@outlook.com>
2022-06-08 11:44:04 +0200

    Fixes #34970 - Move things from Foreman

--- app/controllers/foreman_remote_execution/concerns/api/v2/registration_commands_controller_extensions.rb
+++ app/controllers/foreman_remote_execution/concerns/api/v2/registration_commands_controller_extensions.rb
@@ -9,6 +9,7 @@ module ForemanRemoteExecution
             update_api(:create) do
               param :registration_command, Hash do
                 param :remote_execution_interface, String, desc: N_("Identifier of the Host interface for Remote execution")
+                param :setup_remote_execution_pull, :bool, desc: N_("Set 'host_registration_remote_execution_pull' parameter for the host. If it is set to true, pull provider client will be deployed on the host")
               end
             end
           end
