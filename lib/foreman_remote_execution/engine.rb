f2df3afd4d8ac052db3551f227c52e304bd5bff6
Adam Ruzicka <a.ruzicka@outlook.com>
2022-08-01 12:27:49 +0200

    Fixes #35308 - Make script provider name match proxy feature
    When assigning REX proxies to a subnet, proxy features are matched
    exactly against registered REX provider names. Since there is a one
    letter mismatch, proxies having only Script feature would not show as
    available in the list.

--- lib/foreman_remote_execution/engine.rb
+++ lib/foreman_remote_execution/engine.rb
@@ -334,7 +334,7 @@ module ForemanRemoteExecution
       ForemanTasks::Task.include ForemanRemoteExecution::ForemanTasksTaskExtensions
       ForemanTasks::Cleaner.include ForemanRemoteExecution::ForemanTasksCleanerExtensions
       RemoteExecutionProvider.register(:SSH, ::SSHExecutionProvider)
-      RemoteExecutionProvider.register(:script, ::ScriptExecutionProvider)
+      RemoteExecutionProvider.register(:Script, ::ScriptExecutionProvider)
 
       ForemanRemoteExecution.register_rex_feature
 
