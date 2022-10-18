36879235026a5e776012f3a795b9a4679466f0e8
MariaAga <mariaaga@redhat.com>
2022-10-18 19:15:40 +0200

    Fixes #35644 - Allow host details features open job form

--- webpack/react_app/components/FeaturesDropdown/actions.js
+++ webpack/react_app/components/FeaturesDropdown/actions.js
@@ -7,7 +7,28 @@ export const runFeature = (hostId, feature, label) => dispatch => {
     `/job_invocations?feature=${feature}&host_ids%5B%5D=${hostId}`
   );
 
-  const successToast = () => sprintf(__('%s job has been invoked'), label);
+  const successToast = ({ request }) => {
+    if (request.responseURL.includes('job_invocations?')) {
+      return __('Opening job invocation form');
+    }
+    return sprintf(__('%s job has been invoked'), label);
+  };
   const errorToast = ({ message }) => message;
-  dispatch(post({ key: feature.toUpperCase(), url, successToast, errorToast }));
+  dispatch(
+    post({
+      key: feature.toUpperCase(),
+      url,
+      successToast,
+      errorToast,
+      handleSuccess: ({ request }) => {
+        if (request.responseURL.includes('job_invocations?')) {
+          // checking if user should be redicted to finish setting up the job
+          window.location.href = request.responseURL.replace(
+            'job_invocations?',
+            'job_invocations/new?'
+          );
+        }
+      },
+    })
+  );
 };
