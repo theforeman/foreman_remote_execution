c1a83a6432806d762446ca8b16ddcabdb257152f
Adam Ruzicka <aruzicka@redhat.com>
2022-12-09 14:09:58 +0100

    Fixes #24962 - Make legend clickable

--- webpack/react_app/components/jobInvocations/AggregateStatus/index.js
+++ webpack/react_app/components/jobInvocations/AggregateStatus/index.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AggregateStatus = ({ statuses }) => (
+const AggregateStatus = ({ statuses, chartFilter }) => (
   <div id="aggregate_statuses">
     <p className="card-pf-aggregate-status-notifications">
-      <span className="card-pf-aggregate-status-notification">
+      <a
+        className="card-pf-aggregate-status-notification"
+        onClick={() => chartFilter('success')}
+      >
         <span id="success_count">
           <span className="pficon pficon-ok" />
           {statuses.success}
         </span>
-      </span>
-      <span className="card-pf-aggregate-status-notification">
+      </a>
+
+      <a
+        className="card-pf-aggregate-status-notification"
+        onClick={() => chartFilter('failed')}
+      >
         <span id="failed_count">
           <span className="pficon pficon-error-circle-o" />
           {statuses.failed}
         </span>
-      </span>
-      <span className="card-pf-aggregate-status-notification">
+      </a>
+
+      <a
+        className="card-pf-aggregate-status-notification"
+        onClick={() => chartFilter('pending')}
+      >
         <span id="pending_count">
           <span className="pficon pficon-running" />
           {statuses.pending}
         </span>
-      </span>
-      <span className="card-pf-aggregate-status-notification">
+      </a>
+
+      <a
+        className="card-pf-aggregate-status-notification"
+        onClick={() => chartFilter('cancelled')}
+      >
         <span id="cancelled_count">
           <span className="pficon pficon-close" />
           {statuses.cancelled}
         </span>
-      </span>
+      </a>
     </p>
   </div>
 );
@@ -39,6 +54,7 @@ AggregateStatus.propTypes = {
     pending: PropTypes.number,
     success: PropTypes.number,
   }).isRequired,
+  chartFilter: PropTypes.func.isRequired,
 };
 
 export default AggregateStatus;
