689306046577b5359411f4f682d65276d2a29fef
MariaAga <mariaaga@redhat.com>
2023-01-26 16:44:22 +0100

    Fixes #36010 - add schedule info to review step in wizard

---
+++ webpack/JobWizard/steps/ReviewDetails/helpers.js
@@ -0,0 +1,43 @@
+import { translate as __, sprintf } from 'foremanReact/common/I18n';
+import { getWeekDays } from '../Schedule/RepeatWeek';
+import { repeatTypes } from '../../JobWizardConstants';
+
+export const parseEnd = (ends, isNeverEnds, repeatAmount) => {
+  if (isNeverEnds) {
+    return __('Never');
+  }
+  if (ends) {
+    const endsDate = new Date(ends);
+    return endsDate.toString();
+  }
+  return sprintf(__('After %s occurences'), repeatAmount);
+};
+
+export const parseRepeat = (repeatType, repeatData) => {
+  switch (repeatType) {
+    case repeatTypes.hourly:
+      return sprintf(__('Every hour at minute %s'), repeatData.minute);
+    case repeatTypes.daily:
+      return sprintf(__('Every day at %s'), repeatData.at);
+    case repeatTypes.weekly: {
+      const daysKeys = Object.keys(repeatData.daysOfWeek).filter(
+        k => repeatData.daysOfWeek[k]
+      );
+      const days = getWeekDays()
+        .filter((d, index) => index in daysKeys)
+        .join(', ');
+      return sprintf(__('Every week on %s at %s'), days, repeatData.at);
+    }
+    case repeatTypes.monthly:
+      return sprintf(
+        __('Every month on %s at %s'),
+        repeatData.days,
+        repeatData.at
+      );
+    case repeatTypes.cronline:
+      return `${__('Cron line')} - ${repeatData.cronline}`;
+
+    default:
+      return '';
+  }
+};
