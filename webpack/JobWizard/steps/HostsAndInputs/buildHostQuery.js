9ed7249f6e810f60e1f70006ed3939c9d49d979e
MariaAga <mariaaga@redhat.com>
2023-01-27 06:27:30 +0100

    Fixes #35787 - remove () wrapper in wizard targeting

--- webpack/JobWizard/steps/HostsAndInputs/buildHostQuery.js
+++ webpack/JobWizard/steps/HostsAndInputs/buildHostQuery.js
@@ -1,18 +1,23 @@
 export const buildHostQuery = (selected, search) => {
   const { hosts, hostCollections, hostGroups } = selected;
-  const hostsSearch = `(id ^ (${hosts.map(({ id }) => id).join(',')}))`;
-  const hostCollectionsSearch = `(host_collection_id ^ (${hostCollections
+  const hostsSearch = `id ^ (${hosts.map(({ id }) => id).join(',')})`;
+  const hostCollectionsSearch = `host_collection_id ^ (${hostCollections
     .map(({ id }) => id)
-    .join(',')}))`;
-  const hostGroupsSearch = `(hostgroup_id ^ (${hostGroups
+    .join(',')})`;
+  const hostGroupsSearch = `hostgroup_id ^ (${hostGroups
     .map(({ id }) => id)
-    .join(',')}))`;
-  return [
+    .join(',')})`;
+  const queryParts = [
     hosts.length ? hostsSearch : false,
     hostCollections.length ? hostCollectionsSearch : false,
     hostGroups.length ? hostGroupsSearch : false,
-    search.length ? `(${search})` : false,
-  ]
-    .filter(Boolean)
-    .join(' or ');
+    search.length ? search : false,
+  ].filter(Boolean);
+  if (queryParts.length === 0) {
+    return null;
+  }
+  if (queryParts.length === 1) {
+    return queryParts[0];
+  }
+  return queryParts.map(p => `(${p})`).join(' or ');
 };
