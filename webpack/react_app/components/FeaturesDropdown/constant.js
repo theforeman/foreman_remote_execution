5bf78ebae61d8b35682a30486406f5d0891da75f
MariaAga <mariaaga@redhat.com>
2023-09-15 15:17:02 +0200

    Fixes #36751 - check permissions for schedule job button

--- webpack/react_app/components/FeaturesDropdown/constant.js
+++ webpack/react_app/components/FeaturesDropdown/constant.js
@@ -1,2 +1,3 @@
-export const REX_FEATURES_API = '/api/remote_execution_features';
+export const REX_FEATURES_API = host =>
+  `/api/v2/hosts/${host}/available_remote_execution_features`;
 export const NEW_JOB_PAGE = '/job_invocations/new?host_ids%5B%5D';
