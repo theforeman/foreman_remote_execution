eee80e68e7700b18f556afcb307a88dea164d55f
Jeremy Lenz <jlenz@redhat.com>
2023-10-20 12:29:11 -0400

    Fixes #36851 - Rename migration file so it doesn't break migrations

--- db/migrate/20210517132916_add_host_proxy_invocations.rb
+++ db/migrate/20210517132916_add_host_proxy_invocations.rb
@@ -1,12 +1,12 @@
 class AddHostProxyInvocations < ActiveRecord::Migration[6.0]
   def change
     # rubocop:disable Rails/CreateTableWithTimestamps
-    create_table :host_proxy_invocations do |t|
+    create_table :host_proxy_invocations, if_not_exists: true do |t|
       t.references :host, :null => false
       t.references :smart_proxy, :null => false
     end
     # rubocop:enable Rails/CreateTableWithTimestamps
 
-    add_index :host_proxy_invocations, [:host_id, :smart_proxy_id], unique: true
+    add_index :host_proxy_invocations, [:host_id, :smart_proxy_id], unique: true, if_not_exists: true
   end
 end
