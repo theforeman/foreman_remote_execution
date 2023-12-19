5194ef0cbdf547c8145c42928c4dc1e923e71e40
mariaaga <mariaaga@redhat.com>
2023-12-19 18:05:48 +0000

    Refs #36974 - show run job button to rex users

--- app/helpers/concerns/foreman_remote_execution/job_templates_extensions.rb
+++ app/helpers/concerns/foreman_remote_execution/job_templates_extensions.rb
@@ -4,7 +4,7 @@ module ForemanRemoteExecution
       original = super(template)
 
       if template.is_a?(JobTemplate)
-        original.unshift(display_link_if_authorized(_('Run'), hash_for_new_job_invocation_path(:template_id => template.id).merge(:authorizer => authorizer))) unless template.snippet
+        original.unshift(authorized_for(controller: :job_invocations, action: :create) ? link_to(_('Run'), hash_for_new_job_invocation_path(:template_id => template.id).merge(:authorizer => authorizer)) : nil) unless template.snippet
       end
 
       original
