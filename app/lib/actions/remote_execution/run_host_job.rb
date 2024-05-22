59919d35651da806d57a67a82f96193675313ea0
Adam Ruzicka <aruzicka@redhat.com>
2024-05-22 09:40:46 +0200

    Fixes #37493 - Store smart proxy id in template invocations
    Up until now we only stored it somewhere in the depths of tasks and it
    served us reasonably well, because we only showed the in template
    invocation's details.

--- app/lib/actions/remote_execution/run_host_job.rb
+++ app/lib/actions/remote_execution/run_host_job.rb
@@ -49,7 +49,10 @@ module Actions
         provider_type = provider.proxy_feature
         proxy = determine_proxy!(proxy_selector, provider_type, host)
         link!(proxy)
+        # TODO: Remove this for Foreman 3.16
         input[:proxy_id] = proxy.id
+        template_invocation.smart_proxy_id = proxy.id
+        template_invocation.save!
 
         renderer = InputTemplateRenderer.new(template_invocation.template, host, template_invocation)
         script = renderer.render
