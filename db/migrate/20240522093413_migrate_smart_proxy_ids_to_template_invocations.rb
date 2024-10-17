ebac9f769443e25bfcb5682145a5eeb67bf9812d
Adam Ruzicka <aruzicka@redhat.com>
2024-10-17 09:53:22 +0200

    Fixes #37922 - Handle case where proxy no longer exists
    Fixes e3578d6
    
    (cherry picked from commit cc4aad6ee230d3c92903015436a1e2e75278c45c)

--- db/migrate/20240522093413_migrate_smart_proxy_ids_to_template_invocations.rb
+++ db/migrate/20240522093413_migrate_smart_proxy_ids_to_template_invocations.rb
@@ -1,10 +1,13 @@
 class MigrateSmartProxyIdsToTemplateInvocations < ActiveRecord::Migration[6.0]
   def up
+    proxy_lookup_cache = {}
     ForemanTasks::Link.joins(:task)
                       .where(resource_type: 'SmartProxy', task: { label: 'Actions::RemoteExecution::RunHostJob' })
                       .where.not(resource_id: nil)
                       .find_in_batches do |batch|
       batch.group_by(&:resource_id).each do |resource_id, links|
+        proxy_lookup_cache[resource_id] = SmartProxy.where(id: resource_id).exists? unless proxy_lookup_cache.key?(resource_id)
+        next unless proxy_lookup_cache[resource_id]
         template_invocation_ids = ForemanTasks::Link.where(resource_type: 'TemplateInvocation', task_id: links.map(&:task_id)).select(:resource_id)
         TemplateInvocation.where(id: template_invocation_ids).update_all(smart_proxy_id: resource_id)
       end
