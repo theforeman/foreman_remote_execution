1d93a6703da11f628f351c3415d2c0cf6925f21b
Adam Ruzicka <aruzicka@redhat.com>
2024-11-20 09:17:34 +0000

    Fixes #38021 - Deal with duplicated exit events
    Fixes 6112fbc

--- db/migrate/20240312133027_extend_template_invocation_events.rb
+++ db/migrate/20240312133027_extend_template_invocation_events.rb
@@ -6,6 +6,15 @@ class ExtendTemplateInvocationEvents < ActiveRecord::Migration[6.1]
 
     TemplateInvocationEvent.update_all("external_id = CASE WHEN event_type = 'exit' THEN 'exit' ELSE sequence_id::varchar END")
 
+    # For each template invocation return the lowest id of its exit event, if there is more than 1 exit event
+    scope = TemplateInvocationEvent.select("MIN(id) as id")
+                                   .where(external_id: 'exit')
+                                   .group(:template_invocation_id)
+                                   .having("count(*) > 1")
+
+    # Make sure there is at most one exit event per template invocation
+    TemplateInvocationEvent.where(:id => scope.limit(BATCH_SIZE)).delete_all while scope.any?
+
     remove_index :template_invocation_events, name: :unique_template_invocation_events_index
     remove_column :template_invocation_events, :sequence_id
 
