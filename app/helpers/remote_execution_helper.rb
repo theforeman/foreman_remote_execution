3f4c1817caef4f9a57f5f82f0e4c96985a31ec05
Adam Ruzicka <aruzicka@redhat.com>
2025-01-28 10:53:24 -0500

    Fixes #38167 - Take the redesigned job invocation page out of experimental labs

--- app/helpers/remote_execution_helper.rb
+++ app/helpers/remote_execution_helper.rb
@@ -99,12 +99,11 @@ module RemoteExecutionHelper
         :disabled => !task.cancellable?,
         :method => :post)
     end
-    if Setting[:lab_features]
-      buttons << link_to(_('New UI'), new_job_invocation_detail_path(:id => job_invocation.id),
-        class: 'btn btn-default',
-        title: _('Switch to the new job invocation detail UI'))
-    end
-    return buttons
+    buttons << link_to(_('New UI'), new_job_invocation_detail_path(:id => job_invocation.id),
+      class: 'btn btn-default',
+      title: _('Switch to the new job invocation detail UI'))
+
+    buttons
   end
 
   def template_invocation_task_buttons(task, invocation)
