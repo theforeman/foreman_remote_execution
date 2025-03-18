theforeman/foreman_remote_execution

commit 35cd0649f681e6505a8f357276f507d716736047

Fixes #38187 - Fix kebab items in job invocation (#947)
- Fix kebab items in job invocation
- Change dropdown item element to <a>, add styling, fixed capitalization

webpack/JobInvocationDetail/TemplateInvocationComponents/TemplateActionButtons.js
@@ -7,6 +7,7 @@ import { APIActions } from 'foremanReact/redux/API';
 import { addToast } from 'foremanReact/components/ToastsList';
 import { translate as __ } from 'foremanReact/common/I18n';
 import { selectTemplateInvocation } from '../JobInvocationSelectors';
+import './index.scss';
 
 const actions = ({
   taskID,
@@ -55,7 +56,7 @@ const actions = ({
   },
   abort: {
     name: 'template-invocation-abort-job',
-    text: __('Abort task'),
+    text: __('Abort Task'),
     permission: permissions.cancel_job_invocations,
     onClick: () => {
       dispatch(
@@ -96,17 +97,15 @@ export const RowActions = ({ hostID, jobID }) => {
     permissions,
     dispatch,
   });
+
   const rowActions = Object.values(getActions)
-    .map(({ text, onClick, href, permission, isDisabled }) =>
+    .map(({ text, href, onClick, permission, isDisabled }) =>
       permission
         ? {
-            title: href ? (
-              <a href={href} target="_blank" rel="noreferrer">
-                {text}
-              </a>
-            ) : (
-              text
-            ),
+            title: text,
+            component: 'a',
+            className: 'jobs-table-action-item',
+            href,
             onClick,
             isDisabled,
           }

webpack/JobInvocationDetail/TemplateInvocationComponents/index.scss
@@ -1,0 +1,3 @@
+.jobs-table-action-item:hover {
+    color: var(--pf-c-dropdown__menu-item--Color);
+}

commit c920c3230a401b978579246ca0ac16fc031a5662

Use sentence case for abort button task
The patternfly style guide prefers Sentence case instead of Title Case.

Link: https://www.patternfly.org/ux-writing/capitalization
Fixes: 35cd0649f681 ("Fixes #38187 - Fix kebab items in job invocation (#947)")

webpack/JobInvocationDetail/TemplateInvocationComponents/TemplateActionButtons.js
@@ -56,7 +56,7 @@ const actions = ({
   },
   abort: {
     name: 'template-invocation-abort-job',
-    text: __('Abort Task'),
+    text: __('Abort task'),
     permission: permissions.cancel_job_invocations,
     onClick: () => {
       dispatch(