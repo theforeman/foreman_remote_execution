theforeman/foreman_remote_execution

commit f0235ca3dc6645ee990c6799600fd69a2323f14b

Fixes #37750 - Update to Patternfly 5

webpack/JobWizard/steps/form/FormHelpers.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Popover, Button } from '@patternfly/react-core';
+import { Popover, Button, Icon } from '@patternfly/react-core';
 import { HelpIcon } from '@patternfly/react-icons';
 import { translate as __ } from 'foremanReact/common/I18n';
+import styles from '@patternfly/react-styles/css/components/Form/form';
 
 export const helpLabel = (text, id) => {
   if (!text) return null;
@@ -12,9 +13,11 @@ export const helpLabel = (text, id) => {
         type="button"
         aria-label={__('open-help-tooltip-button')}
         onClick={e => e.preventDefault()}
-        className="pf-c-form__group-label-help"
+        className={styles.formGroupLabelHelp}
       >
-        <HelpIcon noVerticalAlign />
+        <Icon isInline>
+          <HelpIcon />
+        </Icon>
       </button>
     </Popover>
   );
@@ -31,7 +34,7 @@ export const ResetDefault = ({ setValue, defaultValue }) =>
       className="reset-default"
       component="a"
       variant="link"
-      isSmall
+      size="sm"
       onClick={() => setValue(defaultValue)}
     >
       {__('Reset to default')}

webpack/JobInvocationDetail/JobAdditionInfo.js
@@ -69,7 +69,7 @@ const Schedule = ({ data }) => {
   return (
     <ExpandableSection
       toggleText={__('Schedule')}
-      onToggle={setIsExpanded}
+      onToggle={(_event, val) => setIsExpanded(val)}
       isExpanded={isExpanded}
     >
       <DataList isCompact>
@@ -107,7 +107,7 @@ const Recurring = ({ data }) => {
   return (
     <ExpandableSection
       toggleText={__('Recurring logic')}
-      onToggle={setIsExpanded}
+      onToggle={(_event, val) => setIsExpanded(val)}
       isExpanded={isExpanded}
     >
       <DataList isCompact>
@@ -149,7 +149,7 @@ const TargetHosts = ({ data }) => {
   return (
     <ExpandableSection
       toggleText={__('Target Hosts')}
-      onToggle={setIsExpanded}
+      onToggle={(_event, val) => setIsExpanded(val)}
       isExpanded={isExpanded}
     >
       <span>{targetingSelectioning}</span>{' '}
@@ -173,7 +173,7 @@ const Inputs = ({ data }) => {
   return (
     <ExpandableSection
       toggleText={__('User Inputs')}
-      onToggle={setIsExpanded}
+      onToggle={(_event, val) => setIsExpanded(val)}
       isExpanded={isExpanded}
     >
       <DataList isCompact>

webpack/JobInvocationDetail/JobInvocationDetail.scss
@@ -15,22 +15,22 @@
 
     .legend-title {
       font-weight: bold;
-      font-size: var(--pf-global--FontSize--sm);
+      font-size: var(--pf-v5-global--FontSize--sm);
       margin-left: 8px;
       margin-bottom: 0;
     }
 
-    .pf-c-description-list {
+    .pf-v5-c-description-list {
       margin-left: 8px;
       margin-top: 8px;
 
-      .pf-c-description-list__term .pf-c-description-list__text {
+      .pf-v5-c-description-list__term .pf-v5-c-description-list__text {
         font-weight: normal;
       }
     }
   }
 
-  .pf-c-divider {
+  .pf-v5-c-divider {
     max-height: $chart_size;
   }
 
@@ -104,7 +104,7 @@
     margin-top: 10px;
   }
 
-  .pf-c-toggle-group {
+  .pf-v5-c-toggle-group {
     margin-bottom: 10px;
   }
 }

webpack/JobInvocationDetail/JobInvocationHostTable.js
@@ -6,10 +6,10 @@ import { translate as __ } from 'foremanReact/common/I18n';
 import { FormattedMessage } from 'react-intl';
 import { Tr, Td, Tbody, ExpandableRowContent } from '@patternfly/react-table';
 import {
-  Title,
   EmptyState,
   EmptyStateVariant,
   EmptyStateBody,
+  EmptyStateHeader,
 } from '@patternfly/react-core';
 import { foremanUrl } from 'foremanReact/common/helpers';
 import { useAPI } from 'foremanReact/common/hooks/API/APIHooks';
@@ -163,9 +163,10 @@ const JobInvocationHostTable = ({
           <span className="empty-state-icon">
             <Icon name="add-circle-o" type="pf" size="2x" />
           </span>
-          <Title ouiaId="empty-state-header" headingLevel="h5" size="4xl">
-            {__('No Results')}
-          </Title>
+          <EmptyStateHeader
+            titleText={<>{__('No Results')}</>}
+            headingLevel="h5"
+          />
           <EmptyStateBody>
             <div className="empty-state-description">
               {targeting?.targeting_type === 'dynamic_query' ? (
@@ -217,10 +218,12 @@ const JobInvocationHostTable = ({
             setShowAlert={setShowAlert}
             results={results}
             id={id}
+            key="OpenAlInvocations"
           />,
           <JobInvocationHostTableToolbar
             dropdownFilter={selectedFilter}
             setDropdownFilter={wrapSetSelectedFilter}
+            key="JobInvocationHostTableToolbar"
           />,
         ]}
       >

webpack/JobInvocationDetail/JobInvocationHostTableToolbar.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { translate as __ } from 'foremanReact/common/I18n';
-import { Select, SelectOption, SelectList } from '@patternfly/react-core/next'; // remove "/next" after switching to PF5
-import { MenuToggle, ToolbarItem } from '@patternfly/react-core';
+import {
+  MenuToggle,
+  ToolbarItem,
+  Select,
+  SelectOption,
+  SelectList,
+} from '@patternfly/react-core';
 import { STATUS_TITLES } from './JobInvocationConstants';
 
 const JobInvocationHostTableToolbar = ({
@@ -43,7 +48,7 @@ const JobInvocationHostTableToolbar = ({
           {Object.values(STATUS_TITLES).map(result => (
             <SelectOption
               key={result.id}
-              itemId={result.id}
+              value={result.id}
               isSelected={result.id === dropdownFilter}
             >
               {result.title}

webpack/JobInvocationDetail/JobInvocationToolbarButtons.js
@@ -1,16 +1,14 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Button, Split, SplitItem } from '@patternfly/react-core';
 import {
-  Button,
   Dropdown,
   DropdownItem,
   DropdownPosition,
   DropdownSeparator,
   DropdownToggle,
-  Split,
-  SplitItem,
-} from '@patternfly/react-core';
+} from '@patternfly/react-core/deprecated';
 import { translate as __ } from 'foremanReact/common/I18n';
 import { foremanUrl } from 'foremanReact/common/helpers';
 import { STATUS as APIStatus } from 'foremanReact/constants';
@@ -242,7 +240,7 @@ const JobInvocationToolbarButtons = ({
                   </Button>,
                 ]}
                 splitButtonVariant="action"
-                onToggle={setIsActionOpen}
+                onToggle={(_event, val) => setIsActionOpen(val)}
               />
             }
             isOpen={isActionOpen}

webpack/JobInvocationDetail/TemplateInvocationComponents/OutputToggleGroup.js
@@ -97,7 +97,7 @@ export const OutputToggleGroup = ({
                 text={text}
                 buttonId={id}
                 isSelected={isSelected}
-                onChange={onClick}
+                onChange={(_event, val) => onClick(val)}
               />
             )
           )}

webpack/JobInvocationDetail/TemplateInvocationComponents/PreviewTemplate.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  TableComposable,
-  Thead,
-  Tr,
-  Th,
-  Tbody,
-  Td,
-} from '@patternfly/react-table';
+import { Table, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
 import { translate as __ } from 'foremanReact/common/I18n';
 
 export const PreviewTemplate = ({ inputValues }) =>
   inputValues.length ? (
-    <TableComposable
+    <Table
       ouiaId="template-invocation-preview-table"
       isStriped
       variant="compact"
@@ -36,7 +29,7 @@ export const PreviewTemplate = ({ inputValues }) =>
           </Tr>
         ))}
       </Tbody>
-    </TableComposable>
+    </Table>
   ) : (
     <span>{__('No user input')}</span>
   );

commit 0c4e031c67e9dc60162462bd9a84341476a58019

Hardcode form group label help
Fixes: f0235ca3dc6645ee990c6799600fd69a2323f14b

webpack/JobWizard/steps/form/FormHelpers.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Popover, Button, Icon } from '@patternfly/react-core';
 import { HelpIcon } from '@patternfly/react-icons';
 import { translate as __ } from 'foremanReact/common/I18n';
-import styles from '@patternfly/react-styles/css/components/Form/form';
 
 export const helpLabel = (text, id) => {
   if (!text) return null;
@@ -13,7 +12,7 @@ export const helpLabel = (text, id) => {
         type="button"
         aria-label={__('open-help-tooltip-button')}
         onClick={e => e.preventDefault()}
-        className={styles.formGroupLabelHelp}
+        className="pf-v5-c-form__group-label-help"
       >
         <Icon isInline>
           <HelpIcon />