26e12622de2c6d2b6d69f707e154a95e80007540
Jonathon Turel <jturel@gmail.com>
2025-05-27 19:05:13 +0000

    Remove unnecessary inflection

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,6 +1,5 @@
 Rails.autoloaders.each do |autoloader|
   autoloader.inflector.inflect(
-    'script_execution_provider' => 'ScriptExecutionProvider',
     'remote_execution_ssh' => 'RemoteExecutionSSH'
   )
 end
