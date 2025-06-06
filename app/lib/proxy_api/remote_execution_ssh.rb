961df7e86e7c6714faee70f12e8a32b5d8b92247
alazik <alazik@redhat.com>
2025-06-06 15:04:37 +0200

    Fixes #38478 - Introduce SSH CA certificate support

--- app/lib/proxy_api/remote_execution_ssh.rb
+++ app/lib/proxy_api/remote_execution_ssh.rb
@@ -11,6 +11,15 @@ module ::ProxyAPI
       raise ProxyException.new(url, e, N_('Unable to fetch public key'))
     end
 
+    def ca_pubkey
+      get('ca_pubkey')&.strip
+    rescue RestClient::ResourceNotFound => e
+      Rails.logger.warn(format(N_("Unable to fetch CA public key: %{error}"), error: e.message))
+      nil
+    rescue => e
+      raise ProxyException.new(url, e, N_('Unable to fetch CA public key'))
+    end
+
     def drop_from_known_hosts(hostname)
       delete('known_hosts/' + hostname)
     rescue => e
