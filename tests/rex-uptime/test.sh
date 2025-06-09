38e2b8f23b52291ed86c4f1b307c59aa89d54e93
akumari <akumari@redhat.com>
2025-06-09 15:31:43 +0530

    enhance tmt test with smart proxy org setup using hammer

--- tests/rex-uptime/test.sh
+++ tests/rex-uptime/test.sh
@@ -3,8 +3,19 @@ set -euo pipefail
 
 echo "Running remote execution test: uptime"
 
-hammer job-invocation create \
-  --job-template 'Run Command - Script Default' \
-  --inputs 'command=uptime' \
-  --search-query "name ~ $(hostname -f)"
+job_template='Run Command - Script Default'
+SMART_PROXY_NAME=$(hostname -f)
+REX_ORG="Remote Execution Org $$"
 
+OLD_ORGS=$(hammer --output csv --no-headers --show-ids proxy info --name "${SMART_PROXY_NAME}" --fields Organizations)
+
+hammer organization create --name "${REX_ORG}"
+hammer host update --name "${SMART_PROXY_NAME}" --new-organization "${REX_ORG}"
+hammer proxy update --name "${SMART_PROXY_NAME}" --organizations "${OLD_ORGS},${REX_ORG}"
+
+hammer job-invocation create \
+  --job-template "${job_template}" \
+  --inputs 'command=uptime' \
+  --search-query "name = ${SMART_PROXY_NAME}"
+
+echo "Test completed successfully."
