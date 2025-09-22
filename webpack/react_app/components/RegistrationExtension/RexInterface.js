2b495c9a43e29ecdbc612f7f24e46751f8db233c
Konstantinos Familonidis <kfamilon@redhat.com>
2025-09-22 14:14:45 +0300

    Fixes #38714 - use common LabelIcon for popup help
    Refactors the popup help icon to use the shared LabelIcon component.
    The Enzyme tests for this component have been replaced with new tests
    using React Testing Library to align with our current testing standards
    
    Fixes ddc986e (Fixes #32218 - Host Registration - New form)

--- webpack/react_app/components/RegistrationExtension/RexInterface.js
+++ webpack/react_app/components/RegistrationExtension/RexInterface.js
@@ -2,32 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { translate as __ } from 'foremanReact/common/I18n';
+import LabelIcon from 'foremanReact/components/common/LabelIcon';
 
-import { FormGroup, TextInput, Popover, Icon } from '@patternfly/react-core';
-
-import { HelpIcon } from '@patternfly/react-icons';
+import { FormGroup, TextInput } from '@patternfly/react-core';
 
 const RexInterface = ({ isLoading, onChange }) => (
   <FormGroup
     label={__('Remote Execution Interface')}
     fieldId="reg_rex_interface"
     labelIcon={
-      <Popover
-        bodyContent={
-          <div>
-            {__('Identifier of the Host interface for Remote execution')}
-          </div>
-        }
-      >
-        <button
-          className="pf-v5-cform__group-label-help"
-          onClick={e => e.preventDefault()}
-        >
-          <Icon isInline>
-            <HelpIcon />
-          </Icon>
-        </button>
-      </Popover>
+      <LabelIcon
+        text={__('Identifier of the Host interface for Remote execution')}
+      />
     }
   >
     <TextInput
--- webpack/react_app/components/RegistrationExtension/__tests__/RexInterface.test.js
+++ webpack/react_app/components/RegistrationExtension/__tests__/RexInterface.test.js
@@ -1,9 +1,25 @@
-import { testComponentSnapshotsWithFixtures } from '@theforeman/test';
+import React from 'react';
+import { screen, fireEvent, render, act } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import RexInterface from '../RexInterface';
 
 const fixtures = {
   renders: { isLoading: false, onChange: () => {} },
 };
 
-describe('RexInterface', () =>
-  testComponentSnapshotsWithFixtures(RexInterface, fixtures));
+describe('RexInterface', () => {
+  it('should render label with help icon and popover instructions', async () => {
+    jest.useFakeTimers();
+    render(<RexInterface {...fixtures.renders} />);
+    await act(async () => {
+      await fireEvent.click(screen.getByRole('button'));
+
+      jest.advanceTimersByTime(500);
+    });
+
+    expect(screen.getByText('Remote Execution Interface')).toBeInTheDocument();
+    expect(
+      screen.getByText('Identifier of the Host interface for Remote execution')
+    ).toBeInTheDocument();
+  });
+});
