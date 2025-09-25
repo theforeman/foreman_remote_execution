theforeman/foreman_remote_execution

commit ac66cfb9a6b8fdc07cccdb7c4bfc82d0830c1084

Fixes #38599 - Sort out the loading of the new job invocation page

webpack/JobInvocationDetail/index.js
@@ -3,11 +3,13 @@ import {
   Flex,
   PageSection,
   PageSectionVariants,
+  Skeleton,
 } from '@patternfly/react-core';
 import { useAPI } from 'foremanReact/common/hooks/API/APIHooks';
 import { translate as __, documentLocale } from 'foremanReact/common/I18n';
 import { stopInterval } from 'foremanReact/redux/middlewares/IntervalMiddleware';
 import PageLayout from 'foremanReact/routes/common/PageLayout/PageLayout';
+import SkeletonLoader from 'foremanReact/components/common/SkeletonLoader';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -20,6 +22,7 @@ import {
   DATE_OPTIONS,
   JOB_INVOCATION_KEY,
   STATUS,
+  STATUS_UPPERCASE,
 } from './JobInvocationConstants';
 import JobInvocationHostTable from './JobInvocationHostTable';
 import JobInvocationOverview from './JobInvocationOverview';
@@ -38,18 +41,18 @@ const JobInvocationDetailPage = ({
   const items = useSelector(selectItems);
   const {
     description,
-    failed,
+    failed = 0,
     status_label: statusLabel,
     task,
     start_at: startAt,
-    targeting,
+    targeting = {},
   } = items;
   const finished =
     statusLabel === STATUS.FAILED ||
     statusLabel === STATUS.SUCCEEDED ||
     statusLabel === STATUS.CANCELLED;
   const autoRefresh = task?.state === STATUS.PENDING || false;
-  const { response, status } = useAPI(
+  const { response, status: permissionsApiStatus } = useAPI(
     'get',
     currentPermissionsUrl,
     CURRENT_PERMISSIONS
@@ -89,10 +92,22 @@ const JobInvocationDetailPage = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, task?.id]);
 
+  const pageStatus =
+    items.id === undefined
+      ? STATUS_UPPERCASE.PENDING
+      : STATUS_UPPERCASE.RESOLVED;
+
   const breadcrumbOptions = {
     breadcrumbItems: [
       { caption: __('Jobs'), url: `/job_invocations` },
-      { caption: description },
+      {
+        caption:
+          pageStatus === STATUS_UPPERCASE.PENDING ? (
+            <Skeleton width="350px" />
+          ) : (
+            description
+          ),
+      },
     ],
     isPf4: true,
   };
@@ -107,7 +122,7 @@ const JobInvocationDetailPage = ({
             jobId={id}
             data={items}
             currentPermissions={response.results}
-            permissionsStatus={status}
+            permissionsStatus={permissionsApiStatus}
           />
         }
         searchable={false}
@@ -116,12 +131,20 @@ const JobInvocationDetailPage = ({
           className="job-invocation-detail-flex"
           alignItems={{ default: 'alignItemsFlexStart' }}
         >
-          <JobInvocationSystemStatusChart
-            data={items}
-            isAlreadyStarted={isAlreadyStarted}
-            formattedStartDate={formattedStartDate}
-            onFilterChange={handleFilterChange}
-          />
+          <SkeletonLoader
+            status={pageStatus}
+            skeletonProps={{
+              height: 105,
+              width: 375,
+            }}
+          >
+            <JobInvocationSystemStatusChart
+              data={items}
+              isAlreadyStarted={isAlreadyStarted}
+              formattedStartDate={formattedStartDate}
+              onFilterChange={handleFilterChange}
+            />
+          </SkeletonLoader>
           <Divider
             orientation={{
               default: 'vertical',
@@ -131,26 +154,41 @@ const JobInvocationDetailPage = ({
             className="job-overview"
             alignItems={{ default: 'alignItemsCenter' }}
           >
-            <JobInvocationOverview
-              data={items}
-              isAlreadyStarted={isAlreadyStarted}
-              formattedStartDate={formattedStartDate}
-              onFilterChange={handleFilterChange}
-            />
+            <SkeletonLoader
+              status={pageStatus}
+              skeletonProps={{
+                height: 105,
+                width: 270,
+              }}
+            >
+              <JobInvocationOverview
+                data={items}
+                isAlreadyStarted={isAlreadyStarted}
+                formattedStartDate={formattedStartDate}
+              />
+            </SkeletonLoader>
           </Flex>
         </Flex>
         <PageSection
           variant={PageSectionVariants.light}
           className="job-additional-info"
         >
-          {items.id !== undefined && <JobAdditionInfo data={items} />}
+          <SkeletonLoader
+            status={pageStatus}
+            skeletonProps={{ height: 150, width: '100%' }}
+          >
+            <JobAdditionInfo data={items} />
+          </SkeletonLoader>
         </PageSection>
       </PageLayout>
       <PageSection
         variant={PageSectionVariants.light}
         className="job-details-table-section table-section"
       >
-        {items.id !== undefined && (
+        <SkeletonLoader
+          status={pageStatus}
+          skeletonProps={{ height: 400, width: '100%' }}
+        >
           <JobInvocationHostTable
             id={id}
             targeting={targeting}
@@ -160,7 +198,7 @@ const JobInvocationDetailPage = ({
             initialFilter={selectedFilter}
             onFilterUpdate={handleFilterChange}
           />
-        )}
+        </SkeletonLoader>
       </PageSection>
     </>
   );

commit 58bc189e21e679cc805f1ef728be6ca5bb7310a0

Fixes #38773 - Add breadcrumb navigation to job invocation page
Enables users to switch between adjacent jobs while maintaining context.
Updates job details, data table, and charts dynamically when navigating.

Fixes: ac66cfb (Fixes #38599 - Sort out the loading of the new job invocation page)

webpack/JobInvocationDetail/JobInvocationHostTable.js
@@ -197,7 +197,7 @@ const JobInvocationHostTable = ({
       filterApiCall();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [initialFilter, statusLabel]);
+  }, [initialFilter, statusLabel, id]);
 
   const {
     updateSearchQuery: updateSearchQueryBulk,

webpack/JobInvocationDetail/index.js
@@ -35,6 +35,7 @@ const JobInvocationDetailPage = ({
   match: {
     params: { id },
   },
+  history,
 }) => {
   const dispatch = useDispatch();
   const items = useSelector(selectItems);
@@ -94,6 +95,16 @@ const JobInvocationDetailPage = ({
       : STATUS_UPPERCASE.RESOLVED;
 
   const breadcrumbOptions = {
+    isSwitchable: true,
+    onSwitcherItemClick: (e, href) => {
+      e.preventDefault();
+      history.push(href);
+    },
+    resource: {
+      nameField: 'description',
+      resourceUrl: '/api/v2/job_invocations',
+      switcherItemUrl: '/job_invocations/:id',
+    },
     breadcrumbItems: [
       { caption: __('Jobs'), url: `/job_invocations` },
       {
@@ -199,6 +210,7 @@ JobInvocationDetailPage.propTypes = {
       id: PropTypes.string.isRequired,
     }),
   }).isRequired,
+  history: PropTypes.object.isRequired,
 };
 
 export default JobInvocationDetailPage;

webpack/JobInvocationDetail/__tests__/MainInformation.test.js
@@ -83,6 +83,7 @@ jest.mock('../JobInvocationHostTable.js', () => () => (
 const reportTemplateJobId = mockReportTemplatesResponse.results[0].id;
 
 const mockStore = configureMockStore([thunk]);
+const props = { history: { push: jest.fn() } };
 
 describe('JobInvocationDetailPage', () => {
   it('renders main information', async () => {
@@ -91,7 +92,10 @@ describe('JobInvocationDetailPage', () => {
 
     const { container } = render(
       <Provider store={store}>
-        <JobInvocationDetailPage match={{ params: { id: `${jobId}` } }} />
+        <JobInvocationDetailPage
+          match={{ params: { id: `${jobId}` } }}
+          {...props}
+        />
       </Provider>
     );
 
@@ -185,6 +189,7 @@ describe('JobInvocationDetailPage', () => {
       <Provider store={store}>
         <JobInvocationDetailPage
           match={{ params: { id: `${jobInvocationDataScheduled.id}` } }}
+          {...props}
         />
       </Provider>
     );
@@ -201,7 +206,10 @@ describe('JobInvocationDetailPage', () => {
     const store = mockStore(jobInvocationDataRecurring);
     render(
       <Provider store={store}>
-        <JobInvocationDetailPage match={{ params: { id: `${jobId}` } }} />
+        <JobInvocationDetailPage
+          match={{ params: { id: `${jobId}` } }}
+          {...props}
+        />
       </Provider>
     );
 