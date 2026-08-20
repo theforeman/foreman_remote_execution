b893526fdfba7c1eb59f9cea3d8c9705918d77b2
Lukas Zapletal <lzap+git@redhat.com>
2026-08-20 17:49:01 +0200

    Fixes #39673 - Use SeedHelper.format_errors in seeds

--- db/seeds.d/60-ssh_proxy_feature.rb
+++ db/seeds.d/60-ssh_proxy_feature.rb
@@ -1,5 +1,5 @@
 f = Feature.where(:name => 'SSH').first_or_create
-raise "Unable to create proxy feature: #{format_errors f}" if f.nil? || f.errors.any?
+raise "Unable to create proxy feature: #{SeedHelper.format_errors(f)}" if f.nil? || f.errors.any?
 
 f = Feature.where(:name => 'Script').first_or_create
-raise "Unable to create proxy feature: #{format_errors f}" if f.nil? || f.errors.any?
+raise "Unable to create proxy feature: #{SeedHelper.format_errors(f)}" if f.nil? || f.errors.any?
